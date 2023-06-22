caa1ef6a42cae47dd062f0c92dd70bedd574380a
Dawid Pawulski <dawid.pawulski@pwc.com>
2023-06-22 12:04:13 +0200

    Repair HomesBuilt Validation

--- src/HE.InvestmentLoans.BusinessLogic/_LoanApplication/Validation/CompanyStructureValidator.cs
+++ src/HE.InvestmentLoans.BusinessLogic/_LoanApplication/Validation/CompanyStructureValidator.cs
@@ -42,13 +42,33 @@ namespace HE.InvestmentLoans.BusinessLogic._LoanApplication.Validation
 
             RuleSet("HomesBuilt", () =>
             {
-                RuleFor(e => e.HomesBuilt)
-                .NotEmpty()
-                .WithMessage("The amount of homes your organisation has built must be a number")
-                .Matches("^\\d{1,2}[.]\\d*$")
-                .WithMessage("The number of homes your organisation has built in the past 3 years must be 99,999 or less")
-                .Matches("^\\d{1,2}$")
-                .WithMessage("The number of homes your organisation has built must be a whole number");
+                RuleFor(c => c.HomesBuilt)
+                    .Cascade(CascadeMode.Stop)
+                    .NotEmpty().WithMessage("The amount of homes your organisation has built must be a number")
+                    .Must(value =>
+                    {
+                        if ((decimal.TryParse(value.Replace('.', ','), out decimal result) || decimal.TryParse(value.Replace(',', '.'), out result)) && result != Math.Floor(result))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }).WithMessage("The number of homes your organisation has built must be a whole number")
+                    .Must(value =>
+                    {
+                        if (!int.TryParse(value, out var intValue))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }).WithMessage("The amount of homes your organisation has built must be a number")
+                    .Must(value =>
+                    {
+                        if (int.TryParse(value, out var intValue))
+                        {
+                            return value.Length <= 5 && intValue <= 99999;
+                        }
+                        return true;
+                    }).WithMessage("The number of homes your organisation has built in the past 3 years must be 99,999 or less");
             });
 
             RuleSet("CheckAnswers", () =>
