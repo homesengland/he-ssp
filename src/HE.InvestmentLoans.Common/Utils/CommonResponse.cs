cef465d82b88e97fe54245fb0b953756ca5e2646
Dawid Pawulski <dawid.pawulski@pwc.com>
2023-07-13 15:51:08 +0200

    Change to cons strings

---
+++ src/HE.InvestmentLoans.Common/Utils/CommonResponse.cs
@@ -0,0 +1,7 @@
+namespace HE.InvestmentLoans.Common.Utils;
+public class CommonResponse
+{
+    public const string Yes = "Yes";
+    public const string No = "No";
+    public const string DoNotKnow = "doNotKnow";
+}
