52259a682517eeba7061e44cd6663bd0bbe99cc8
Pawel Koprowski <pawel.koprowski@pwc.com>
2023-07-17 09:22:21 +0200

    Refactoring fix

--- src/HE.InvestmentLoans.Common/Models/App/IRedisConfig.cs
+++ src/HE.InvestmentLoans.Common/Models/App/IRedisConfig.cs
@@ -2,7 +2,7 @@ namespace HE.InvestmentLoans.Common.Models.App;
 
 public interface IRedisConfig
 {
-    public string ConnectionString { get; set; }
+    public string? ConnectionString { get; set; }
 
     public int ExpireMinutes { get; set; }
 }
