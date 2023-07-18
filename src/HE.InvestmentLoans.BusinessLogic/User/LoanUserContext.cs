d3dd337d289f80784e71fe99391e9750db6557a1
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-07-18 14:58:35 +0200

    LoadUserDetails fix

--- src/HE.InvestmentLoans.BusinessLogic/User/LoanUserContext.cs
+++ src/HE.InvestmentLoans.BusinessLogic/User/LoanUserContext.cs
@@ -53,8 +53,8 @@ public class LoanUserContext : ILoanUserContext
 
     private async Task<Task> LoadUserDetails()
     {
-        var userDetails = await _cacheService.GetValue($"{nameof(this.LoadUserDetails)}_{_userContext.UserGlobalId}", () => _loanUserRepository.GetUserDetails(_userContext.UserGlobalId, _userContext.Email));
-        _accountIds.AddRange(userDetails?.contactRoles.OrderBy(x => x.accountId).Select(x => x.accountId).ToList());
+        var userDetails = _cacheService.GetValue($"{nameof(this.LoadUserDetails)}_{_userContext.UserGlobalId}", () => _loanUserRepository.GetUserDetails(_userContext.UserGlobalId, _userContext.Email ?? null));
+        _accountIds.AddRange(userDetails?.Result?.contactRoles.OrderBy(x => x.accountId).Select(x => x.accountId).ToList());
         _selectedAccountId = _accountIds.FirstOrDefault(Guid.Parse("429d11ab-15fe-ed11-8f6c-002248c653e1"));
 
         if (_selectedAccountId is null)
