cd4444e756c1d83f9e70feabcf3739cf04ec4ae3
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-08-08 15:43:23 +0200

    first path correct

--- src/HE.CRM.Plugins.Tests/CustomApis/CustomApiCallingPathTest.cs
+++ src/HE.CRM.Plugins.Tests/CustomApis/CustomApiCallingPathTest.cs
@@ -1,4 +1,5 @@
 using DataverseModel;
+using FakeItEasy;
 using FakeXrmEasy;
 using FakeXrmEasy.Extensions;
 using HE.Common.IntegrationModel.PortalIntegrationModel;
@@ -20,6 +21,9 @@ namespace HE.CRM.Plugins.Tests.CustomApis
         private XrmFakedContext fakedContext;
         private XrmFakedPluginExecutionContext pluginContext;
 
+        private LoanApplicationDto applicationDto;
+        private SiteDetailsDto siteDetailsDto;
+
         [TestInitialize]
         public void Initialize()
         {
@@ -39,6 +43,8 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             fakedContext.InitializeMetadata(entityMetadata);
             entityMetadata.LogicalName = "invln_loanapplication";
             fakedContext.InitializeMetadata(entityMetadata);
+
+            this.Init();
         }
 
         [TestMethod]
@@ -92,18 +98,103 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             fakedContext.SetEntityMetadata(metadata);
             contact.KeyAttributes.Add(nameof(Contact.invln_externalid), contact.invln_externalid);
 
-            var GetContactRoleOutput = this.CallGetContactRolePlugin(contact.invln_externalid, portal.invln_Portal.Value.ToString(), contact.EMailAddress1);
-            Assert.IsNotNull(GetContactRoleOutput);
-            Assert.IsNotNull(GetContactRoleOutput.contactRoles);
-            Assert.AreEqual(role.invln_Name, GetContactRoleOutput.contactRoles.First().webRoleName);
+            var getContactRoleOutput = this.CallGetContactRolePlugin(contact.invln_externalid, portal.invln_Portal.Value.ToString(), contact.EMailAddress1, false);
+            Assert.IsNotNull(getContactRoleOutput);
+            Assert.IsNotNull(getContactRoleOutput.contactRoles);
 
-            var GetOrganizationDetailsOutput = this.CallGetOrganizationDetailsPlugin(account.Id.ToString(), contact.invln_externalid);
+            Assert.AreEqual(role.invln_Name, getContactRoleOutput.contactRoles.First().webRoleName);
 
-            Assert.IsNotNull(GetOrganizationDetailsOutput);
-            Assert.AreEqual(account.Name, GetOrganizationDetailsOutput.registeredCompanyName);
+            var getOrganizationDetailsOutput = this.CallGetOrganizationDetailsPlugin(account.Id.ToString(), contact.invln_externalid, false);
+
+            Assert.IsNotNull(getOrganizationDetailsOutput);
+            Assert.AreEqual(account.Name, getOrganizationDetailsOutput.registeredCompanyName);
+
+            var sendInvestmentLoanDataOutput = this.CallSendInvestmentsLoanDataToCrm(getContactRoleOutput.externalId, account.Id.ToString(), String.Empty, JsonSerializer.Serialize(applicationDto), false);
+
+            Assert.IsNotNull(sendInvestmentLoanDataOutput);
+            A.CallTo(() => fakedContext.GetOrganizationService().Create(A<invln_Loanapplication>.Ignored)).MustHaveHappened();
+            A.CallTo(() => fakedContext.GetOrganizationService().Create(A<invln_SiteDetails>.Ignored)).MustHaveHappened();
+
+            var getLoanApplicationOutput1 = this.CallGetSingleLoanApplicationPlugin(account.Id.ToString(), getContactRoleOutput.externalId, sendInvestmentLoanDataOutput, false);
+
+            Assert.IsNotNull(getLoanApplicationOutput1);
+            Assert.AreEqual(getLoanApplicationOutput1.name, applicationDto.name);
+
+            var loanDtoNewFieldsValues = new LoanApplicationDto()
+            {
+                projectAbnormalCostsInformation = "projectAbnormalCostsInformationchangetest",
+            };
+
+            this.CallUpdateSingleLoanApplication(JsonSerializer.Serialize(loanDtoNewFieldsValues), "invln_projectabnormalcostsinformation", getLoanApplicationOutput1.loanApplicationId, account.Id.ToString(), getContactRoleOutput.externalId, false);
+            A.CallTo(() => fakedContext.GetOrganizationService().Update(A<invln_Loanapplication>.That.Matches(x => x.Id.ToString() == getLoanApplicationOutput1.loanApplicationId))).MustHaveHappened();
+
+            this.CallUpdateExternalStatus(getLoanApplicationOutput1.loanApplicationId, (int)invln_ExternalStatus.CPssatisfied, false);
+
+            var getLoanApplicationOutput2 = this.CallGetSingleLoanApplicationPlugin(account.Id.ToString(), getContactRoleOutput.externalId, sendInvestmentLoanDataOutput, false);
+            Assert.AreEqual(getLoanApplicationOutput1.loanApplicationId, getLoanApplicationOutput2.loanApplicationId);
+            Assert.AreNotEqual(getLoanApplicationOutput1.loanApplicationExternalStatus, getLoanApplicationOutput2.loanApplicationExternalStatus);
         }
 
-        private List<LoanApplicationDto> CallGetLoanApplicationForAccountAndcontact(string accountId, string externalContactId)
+        private void CallUpdateExternalStatus(string loanApplicationId, int externalStatus, bool shouldThrowError)
+        {
+            Exception exception = null;
+            try
+            {
+                var request = new invln_changeloanapplicationexternalstatusRequest();
+                pluginContext.InputParameters = new ParameterCollection
+                {
+                    {nameof(request.invln_loanapplicationid), loanApplicationId },
+                    {nameof(request.invln_statusexternal), externalStatus }
+                };
+
+                fakedContext.ExecutePluginWithConfigurations<ChangeLoanApplicationExternalStatusPlugin>(pluginContext, "", "");
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (shouldThrowError)
+            {
+                Assert.IsNotNull(exception);
+            }
+            else
+            {
+                Assert.IsNull(exception);
+            }
+        }
+
+        private void CallUpdateSingleLoanApplication(string loanApplication, string fieldsToUpdate, string loanApplicationId, string accountId, string contactExternalId, bool shouldThrowError)
+        {
+            Exception exception = null;
+            try
+            {
+                var request = new invln_updatesingleloanapplicationRequest();
+                pluginContext.InputParameters = new ParameterCollection
+                {
+                    {nameof(request.invln_loanapplication), loanApplication },
+                    {nameof(request.invln_fieldstoupdate), fieldsToUpdate },
+                    {nameof(request.invln_loanapplicationid), loanApplicationId },
+                    {nameof(request.invln_accountid), accountId },
+                    {nameof(request.invln_contactexternalid), contactExternalId },
+                };
+
+                fakedContext.ExecutePluginWithConfigurations<UpdateSingleLoanApplicationPlugin>(pluginContext, "", "");
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (shouldThrowError)
+            {
+                Assert.IsNotNull(exception);
+            }
+            else
+            {
+                Assert.IsNull(exception);
+            }
+        }
+
+        private List<LoanApplicationDto> CallGetLoanApplicationForAccountAndcontact(string accountId, string externalContactId, bool shouldThrowError)
         {
             Exception exception = null;
             try
@@ -121,13 +212,21 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             {
                 exception = ex;
             }
-            var deserializedLoanApplicationDtoList = JsonSerializer.Deserialize<List<LoanApplicationDto>>(pluginContext.OutputParameters.Values.ElementAt(0).ToString());
+            if (shouldThrowError)
+            {
+                Assert.IsNotNull(exception);
+                return null;
+            }
+            else
+            {
+                var deserializedLoanApplicationDtoList = JsonSerializer.Deserialize<List<LoanApplicationDto>>(pluginContext.OutputParameters["invln_loanapplications"].ToString());
 
-            Assert.IsNull(exception);
-            return deserializedLoanApplicationDtoList;
+                Assert.IsNull(exception);
+                return deserializedLoanApplicationDtoList;
+            }
         }
 
-        private LoanApplicationDto CallGetSingleLoanApplicationPlugin(string accountId, string externalContactId, string loanApplicationId)
+        private LoanApplicationDto CallGetSingleLoanApplicationPlugin(string accountId, string externalContactId, string loanApplicationId, bool shouldThrowError)
         {
             Exception exception = null;
             try
@@ -146,13 +245,21 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             {
                 exception = ex;
             }
-            var deserializedLoanApplicationDtoList = JsonSerializer.Deserialize<List<LoanApplicationDto>>(pluginContext.OutputParameters.Values.ElementAt(0).ToString());
+            if (shouldThrowError)
+            {
+                Assert.IsNotNull(exception);
+                return null;
+            }
+            else
+            {
+                var deserializedLoanApplicationDtoList = JsonSerializer.Deserialize<List<LoanApplicationDto>>(pluginContext.OutputParameters["invln_loanapplication"].ToString());
 
-            Assert.IsNull(exception);
-            return deserializedLoanApplicationDtoList?.First();
+                Assert.IsNull(exception);
+                return deserializedLoanApplicationDtoList?.First();
+            }
         }
 
-        private OrganizationDetailsDto CallGetOrganizationDetailsPlugin(string accountId, string externalContactId)
+        private OrganizationDetailsDto CallGetOrganizationDetailsPlugin(string accountId, string externalContactId, bool shouldThrowError)
         {
             Exception exception = null;
             try
@@ -170,13 +277,21 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             {
                 exception = ex;
             }
-            var deserializedOrganizationDetails = JsonSerializer.Deserialize<OrganizationDetailsDto>(pluginContext.OutputParameters.Values.ElementAt(0).ToString());
+            if (shouldThrowError)
+            {
+                Assert.IsNotNull(exception);
+                return null;
+            }
+            else
+            {
+                var deserializedOrganizationDetails = JsonSerializer.Deserialize<OrganizationDetailsDto>(pluginContext.OutputParameters["invln_organizationdetails"].ToString());
 
-            Assert.IsNull(exception);
-            return deserializedOrganizationDetails;
+                Assert.IsNull(exception);
+                return deserializedOrganizationDetails;
+            }
         }
 
-        private ContactRolesDto CallGetContactRolePlugin(string contactExternalId, string portalType, string emailAddress)
+        private ContactRolesDto CallGetContactRolePlugin(string contactExternalId, string portalType, string emailAddress, bool shouldThrowErrors)
         {
             Exception exception = null;
             try
@@ -195,14 +310,22 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             {
                 exception = ex;
             }
-            var deserializedContactRoles = JsonSerializer.Deserialize<ContactRolesDto>(pluginContext.OutputParameters.Values.ElementAt(0).ToString());
 
-            Assert.IsNull(exception);
-            return deserializedContactRoles;
-            //Assert.AreEqual(deserializedContactRoles.contactRoles.ElementAt(0).webRoleName, role.invln_Name);
+            if (shouldThrowErrors)
+            {
+                Assert.IsNotNull(exception);
+                return null;
+            }
+            else
+            {
+                var deserializedContactRoles = JsonSerializer.Deserialize<ContactRolesDto>(pluginContext.OutputParameters["invln_portalroles"].ToString());
+
+                Assert.IsNull(exception);
+                return deserializedContactRoles;
+            }
         }
 
-        private string CallSendInvestmentsLoanDataToCrm(string contactExternalId, string accountId, string loanApplicationId, string requestStringMessage)
+        private string CallSendInvestmentsLoanDataToCrm(string contactExternalId, string accountId, string loanApplicationId, string requestStringMessage, bool shouldThrowError)
         {
             Exception exception = null;
             try
@@ -222,10 +345,84 @@ namespace HE.CRM.Plugins.Tests.CustomApis
             {
                 exception = ex;
             }
-            var loanAppId = pluginContext.OutputParameters.Values.ElementAt(0).ToString();
+            if (shouldThrowError)
+            {
+                Assert.IsNotNull(exception);
+                return String.Empty;
+            }
+            else
+            {
+                var loanAppId = pluginContext.OutputParameters["invln_loanapplicationid"].ToString();
+                Assert.IsNull(exception);
+                return loanAppId;
+            }
+        }
 
-            Assert.IsNull(exception);
-            return loanAppId;
+        private void Init()
+        {
+            siteDetailsDto = new SiteDetailsDto()
+            {
+                currentValue = "value",
+                dateOfPurchase = DateTime.Now,
+                existingLegalCharges = "existingLegalCharges",
+                existingLegalChargesInformation = "existingLegalChargesInformation",
+                haveAPlanningReferenceNumber = "haveAPlanningReferenceNumber",
+                howMuch = "howMuch",
+                landRegistryTitleNumber = "landRegistryTitleNumber",
+                Name = "Name",
+                nameOfGrantFund = "nameOfGrantFund",
+                numberOfAffordableHomes = "numberOfAffordableHomes",
+                numberOfHomes = "numberOfHomes",
+                otherTypeOfHomes = "otherTypeOfHomes",
+                planningReferenceNumber = "planningReferenceNumber",
+                publicSectorFunding = "publicSectorFunding",
+                reason = "reason",
+                siteCoordinates = "siteCoordinates",
+                siteCost = "siteCost",
+                siteName = "siteName",
+                siteOwnership = "siteOwnership",
+                typeOfHomes = new string[] { "typeOfHomes" },
+                typeOfSite = "typeOfSite",
+                valuationSource = "valuationSource",
+                whoProvided = "whoProvided",
+            };
+
+            applicationDto = new LoanApplicationDto()
+            {
+                companyPurpose = "true",
+                existingCompany = "",
+                companyExperience = 5,
+
+                projectGdv = "22.2",
+                projectEstimatedTotalCost = "33.3",
+                projectAbnormalCosts = "true",
+                projectAbnormalCostsInformation = "projectAbnormalCostsInformation",
+                privateSectorApproach = "false",
+                privateSectorApproachInformation = "privateSectorApproachInformation",
+                additionalProjects = "true",
+                refinanceRepayment = "",
+                refinanceRepaymentDetails = "refinanceRepaymentDetails",
+
+                outstandingLegalChargesOrDebt = "false",
+                debentureHolder = "debentureHolder",
+                directorLoans = "true",
+                confirmationDirectorLoansCanBeSubordinated = "false",
+                reasonForDirectorLoanNotSubordinated = "true",
+
+                siteDetailsList = new List<SiteDetailsDto> { siteDetailsDto },
+
+                name = "name",
+                numberOfSites = "20",
+                companyStructureInformation = "companyStructureInformation",
+                costsForAdditionalProjects = "",
+                fundingReason = "Buildinginfrastructureonly",
+                fundingTypeForAdditionalProjects = "fundingTypeForAdditionalProjects",
+                contactEmailAdress = "test@test.pl",
+                LoanApplicationContact = new UserAccountDto()
+                {
+                    ContactExternalId = "2137"
+                }
+            };
         }
     }
 }
