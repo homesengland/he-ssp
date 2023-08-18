6f4c1629684d99f965ce826539f33da68b51e645
Jakub Lotko <jakub.lotko@pwc.com>
2023-08-18 14:42:52 +0200

    Fix horizontal list alignemnt

--- src/HE.InvestmentLoans.BusinessLogic/LoanApplication/CommandHandlers/SubmitLoanApplicationCommandHandler.cs
+++ src/HE.InvestmentLoans.BusinessLogic/LoanApplication/CommandHandlers/SubmitLoanApplicationCommandHandler.cs
@@ -1,7 +1,10 @@
 using HE.InvestmentLoans.BusinessLogic.LoanApplication.Repositories;
 using HE.InvestmentLoans.BusinessLogic.User;
+using HE.InvestmentLoans.BusinessLogic.ViewModel;
+using HE.InvestmentLoans.Common.Extensions;
 using HE.InvestmentLoans.Contract.Application.Commands;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace HE.InvestmentLoans.BusinessLogic.LoanApplication.CommandHandlers;
 
@@ -11,18 +14,27 @@ public class SubmitLoanApplicationCommandHandler : IRequestHandler<SubmitLoanApp
     private readonly ICanSubmitLoanApplication _canSubmitLoanApplication;
 
     private readonly ILoanUserContext _loanUserContext;
+    private readonly IHttpContextAccessor _contextAccessor;
 
-    public SubmitLoanApplicationCommandHandler(ILoanApplicationRepository loanApplicationRepository, ILoanUserContext loanUserContext, ICanSubmitLoanApplication canSubmitLoanApplication)
+    public SubmitLoanApplicationCommandHandler(ILoanApplicationRepository loanApplicationRepository, ILoanUserContext loanUserContext, ICanSubmitLoanApplication canSubmitLoanApplication, IHttpContextAccessor contextAccessor)
     {
         _loanApplicationRepository = loanApplicationRepository;
         _loanUserContext = loanUserContext;
         _canSubmitLoanApplication = canSubmitLoanApplication;
+        _contextAccessor = contextAccessor;
     }
 
     public async Task Handle(SubmitLoanApplicationCommand request, CancellationToken cancellationToken)
     {
         var loanApplication = await _loanApplicationRepository
                                 .GetLoanApplication(request.LoanApplicationId, await _loanUserContext.GetSelectedAccount(), cancellationToken);
+
+        var sessionModel = _contextAccessor.HttpContext?.Session.Get<LoanApplicationViewModel>(request.LoanApplicationId.ToString());
+
+        if (sessionModel != null)
+        {
+            loanApplication.LegacyModel.UseSectionsFrom(sessionModel);
+        }
 
         await loanApplication.Submit(_canSubmitLoanApplication, cancellationToken);
     }
