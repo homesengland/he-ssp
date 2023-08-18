517c2251e4838ab6eaa388b802bda9fe000ee312
PiotrSamb <133100401+PiotrSamb@users.noreply.github.com>
2023-08-18 09:05:19 +0200

    Change app status on owner change (#168)
    Co-authored-by: Piotr Samborski <piotr.samborski@pwc.com>

--- src/HE.CRM.Plugins/Services/LoanApplication/ILoanApplicationService.cs
+++ src/HE.CRM.Plugins/Services/LoanApplication/ILoanApplicationService.cs
@@ -1,10 +1,13 @@
+using DataverseModel;
 using HE.Base.Services;
+using DataverseModel;
 
 namespace HE.CRM.Plugins.Services.LoanApplication
 {
     public interface ILoanApplicationService : ICrmService
     {
         string CreateRecordFromPortal(string contactExternalId, string accountId, string loanApplicationId, string loanApplicationPayload);
+        void ChangeLoanApplicationStatusOnOwnerChange(invln_Loanapplication target, invln_Loanapplication preImage, invln_Loanapplication postImage);
         string GetLoanApplicationsForAccountAndContact(string externalContactId, string accountId, string loanApplicationId = null);
         void ChangeLoanApplicationExternalStatus(int externalStatus, string loanApplicationId);
         void UpdateLoanApplication(string loanApplicationId, string loanApplication, string fieldsToUpdate, string accountId, string contactExternalId);
--- src/HE.CRM.Plugins/Services/LoanApplication/LoanApplicationService.cs
+++ src/HE.CRM.Plugins/Services/LoanApplication/LoanApplicationService.cs
@@ -73,6 +73,19 @@ namespace HE.CRM.Plugins.Services.LoanApplication
             return JsonSerializer.Serialize(entityCollection);
         }
 
+        public void ChangeLoanApplicationStatusOnOwnerChange(invln_Loanapplication target, invln_Loanapplication preImage, invln_Loanapplication postImage)
+        {
+            if (preImage?.StatusCode.Value == (int)invln_Loanapplication_StatusCode.ApplicationSubmitted && preImage.OwnerId.Id != postImage.OwnerId.Id)
+            {
+                loanApplicationRepository.Update(new invln_Loanapplication()
+                {
+                    Id = target.Id,
+                    invln_ExternalStatus = new OptionSetValue((int)invln_ExternalStatus.Underreview),
+                    StatusCode = new OptionSetValue((int)invln_Loanapplication_StatusCode.Underreview)
+                });
+            }
+        }
+
         public string CreateRecordFromPortal(string contactExternalId, string accountId, string loanApplicationId, string loanApplicationPayload)
         {
             this.TracingService.Trace("PAYLOAD:" + loanApplicationPayload);
