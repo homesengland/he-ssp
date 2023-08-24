ef826455a3b7daeca71ddae1d88031b716bcb5fe
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-08-24 12:57:00 +0200

    delete earlybounds

--- src/HE.Investments.Organisation/CrmRepository/IOrganizationRepository.cs
+++ src/HE.Investments.Organisation/CrmRepository/IOrganizationRepository.cs
@@ -1,8 +1,4 @@
-using HE.Investments.Organisation.Model;
-using Microsoft.Xrm.Sdk;
-
 namespace HE.Investments.Organisation.CrmRepository;
 public interface IOrganizationRepository
 {
-    IEnumerable<Account> GetAccountsByOrganizationNameAndCompanyHouseNumber(IOrganizationService service, string organizationName, string companyHouseNumber);
 }
