c376128414836e74415aea5790300df686564c2e
burzynskijakub <133094318+burzynskijakub@users.noreply.github.com>
2023-08-24 13:07:04 +0200

    delete earlybounds (#188)

--- src/HE.Investments.Organisation/CrmRepository/OrganizationRepository.cs
+++ src/HE.Investments.Organisation/CrmRepository/OrganizationRepository.cs
@@ -1,14 +1,4 @@
-using HE.Investments.Organisation.Model;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Client;
-
 namespace HE.Investments.Organisation.CrmRepository;
 public class OrganizationRepository : IOrganizationRepository
 {
-    public IEnumerable<Account> GetAccountsByOrganizationNameAndCompanyHouseNumber(IOrganizationService service, string organizationName, string companyHouseNumber)
-    {
-        using var ctx = new OrganizationServiceContext(service);
-        return ctx.CreateQuery<Account>()
-            .Where(x => x.Name.Contains(organizationName) || x.he_CompaniesHouseNumber == companyHouseNumber).AsEnumerable();
-    }
 }
