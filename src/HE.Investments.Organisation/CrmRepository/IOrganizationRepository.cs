6477c2334e904cdc23d18f2305405088b04d87e3
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-08-28 12:02:48 +0200

    search completed

--- src/HE.Investments.Organisation/CrmRepository/IOrganizationRepository.cs
+++ src/HE.Investments.Organisation/CrmRepository/IOrganizationRepository.cs
@@ -1,7 +1,9 @@
 using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
 
 namespace HE.Investments.Organisation.CrmRepository;
 public interface IOrganizationRepository
 {
     Guid? EnsureCreateOrganization(IOrganizationServiceAsync2 service, string companyNumber, string companyName);
+    EntityCollection SearchForOrganizations(IOrganizationServiceAsync2 service, List<string> organizationNumbers);
 }
