98eee8f514cc0d257ceed8f37319915fadf67f61
Dawid Pawulski <dawid.pawulski@pwc.com>
2023-08-30 11:36:17 +0200

    Add feature flag for ProfileDetails

--- src/HE.InvestmentLoans.BusinessLogic/LoansFeatureFlags.cs
+++ src/HE.InvestmentLoans.BusinessLogic/LoansFeatureFlags.cs
@@ -3,4 +3,5 @@ namespace HE.InvestmentLoans.BusinessLogic;
 public static class LoansFeatureFlags
 {
     public const string SaveApplicationDraftInCrm = nameof(SaveApplicationDraftInCrm);
+    public const string DisplayProfileDetailsPage = nameof(SaveApplicationDraftInCrm);
 }
