e37fc6137e65612f8b840517339015535f6f6aa8
Łukasz Ciura <lukasz.ciura@pwc.com>
2023-08-31 12:52:41 +0200

    #73957 Company Structure - Unit Tests

---
+++ src/HE.InvestmentLoans.BusinessLogic.Tests/User/UserAccountTestData.cs
@@ -0,0 +1,10 @@
+using HE.InvestmentLoans.BusinessLogic.User.Entities;
+using HE.InvestmentLoans.Common.Tests.TestData;
+using HE.InvestmentLoans.Contract.User.ValueObjects;
+
+namespace HE.InvestmentLoans.BusinessLogic.Tests.User;
+
+public static class UserAccountTestData
+{
+    public static readonly UserAccount UserAccountOne = new(UserGlobalId.From("UserOne"), "User@one.com", GuidTestData.GuidTwo, "AccountOne");
+}
