78e96327eb0d316df95fc114c431953a31d694ab
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2023-08-31 16:15:48 +0200

    Switch to crm sdk for profile details (#221)

--- src/HE.InvestmentLoans.BusinessLogic/User/Repositories/LoanUserRepository.cs
+++ src/HE.InvestmentLoans.BusinessLogic/User/Repositories/LoanUserRepository.cs
@@ -1,6 +1,4 @@
-using System.Text.Json;
 using HE.Common.IntegrationModel.PortalIntegrationModel;
-using HE.InvestmentLoans.BusinessLogic.User.Entities;
 using HE.InvestmentLoans.Common.CrmCommunication.Serialization;
 using HE.InvestmentLoans.Contract.Exceptions;
 using HE.InvestmentLoans.Contract.User.ValueObjects;
@@ -37,51 +35,5 @@ public class LoanUserRepository : ILoanUserRepository
         }
 
         return null;
-    }
-
-    public async Task<UserDetails> GetUserDetails(UserGlobalId userGlobalId)
-    {
-        var req = new invln_returnuserprofileRequest()
-        {
-            invln_contactexternalid = userGlobalId.ToString(),
-        };
-
-        var resp_async = await _serviceClient.ExecuteAsync(req);
-        var resp = resp_async != null ? (invln_returnuserprofileResponse)resp_async : throw new NotFoundException(nameof(UserDetails), userGlobalId.ToString());
-
-        var contactDto = CrmResponseSerializer.Deserialize<ContactDto>(resp.invln_userprofile) ?? throw new NotFoundException(nameof(UserDetails), userGlobalId.ToString());
-
-        return new UserDetails(
-            contactDto.firstName,
-            contactDto.lastName,
-            contactDto.jobTitle,
-            contactDto.email,
-            contactDto.phoneNumber,
-            contactDto.secondaryPhoneNumber,
-            contactDto.isTermsAndConditionsAccepted);
-    }
-
-    public async Task SaveAsync(UserDetails userDetails, UserGlobalId userGlobalId, CancellationToken cancellationToken)
-    {
-        var contactDto = new ContactDto
-        {
-            firstName = userDetails.FirstName,
-            lastName = userDetails.Surname,
-            jobTitle = userDetails.JobTitle,
-            email = userDetails.Email,
-            phoneNumber = userDetails.TelephoneNumber,
-            secondaryPhoneNumber = userDetails.SecondaryTelephoneNumber,
-            isTermsAndConditionsAccepted = userDetails.IsTermsAndConditionsAccepted,
-        };
-
-        var contactDtoSerialized = JsonSerializer.Serialize(contactDto);
-
-        var req = new invln_updateuserprofileRequest
-        {
-            invln_contactexternalid = userGlobalId.ToString(),
-            invln_contact = contactDtoSerialized,
-        };
-
-        await _serviceClient.ExecuteAsync(req, cancellationToken);
     }
 }
--- src/HE.InvestmentLoans.BusinessLogic/User/UserDetailsMapper.cs
+++ src/HE.InvestmentLoans.BusinessLogic/User/UserDetailsMapper.cs
@@ -1,11 +1,15 @@
+extern alias Org;
+
 using HE.InvestmentLoans.BusinessLogic.User.Entities;
 using HE.InvestmentLoans.Common.Utils.Constants.FormOption;
 using HE.InvestmentLoans.Contract.User;
+using HE.InvestmentLoans.Contract.User.Commands;
+using Org::HE.Common.IntegrationModel.PortalIntegrationModel;
 
 namespace HE.InvestmentLoans.BusinessLogic.User;
 public static class UserDetailsMapper
 {
-    public static UserDetailsViewModel MapToViewModel(UserDetails userDetailsEntity)
+    public static UserDetailsViewModel MapUserDetailsToViewModel(UserDetails userDetailsEntity)
     {
         return new UserDetailsViewModel()
         {
@@ -16,5 +20,45 @@ public static class UserDetailsMapper
             SecondaryTelephoneNumber = userDetailsEntity.SecondaryTelephoneNumber,
             IsTermsAndConditionsAccepted = userDetailsEntity.IsTermsAndConditionsAccepted == true ? CommonResponse.Checked : null,
         };
+    }
+
+    public static ContactDto MapProvideUserDetailsCommandToContactDto(ProvideUserDetailsCommand provideUserDetailsCommand, string email)
+    {
+        return new ContactDto
+        {
+            firstName = provideUserDetailsCommand.FirstName,
+            lastName = provideUserDetailsCommand.Surname,
+            jobTitle = provideUserDetailsCommand.JobTitle,
+            email = email,
+            phoneNumber = provideUserDetailsCommand.TelephoneNumber,
+            secondaryPhoneNumber = provideUserDetailsCommand.SecondaryTelephoneNumber,
+            isTermsAndConditionsAccepted = provideUserDetailsCommand.IsTermsAndConditionsAccepted == CommonResponse.Checked,
+        };
+    }
+
+    public static ContactDto MapUserDetailsToContactDto(UserDetails userDetails)
+    {
+        return new ContactDto
+        {
+            firstName = userDetails.FirstName,
+            lastName = userDetails.Surname,
+            jobTitle = userDetails.JobTitle,
+            email = userDetails.Email,
+            phoneNumber = userDetails.TelephoneNumber,
+            secondaryPhoneNumber = userDetails.SecondaryTelephoneNumber,
+            isTermsAndConditionsAccepted = userDetails.IsTermsAndConditionsAccepted,
+        };
+    }
+
+    public static UserDetails MapContactDtoToUserDetails(ContactDto contactDto)
+    {
+        return new UserDetails(
+            contactDto.firstName,
+            contactDto.lastName,
+            contactDto.jobTitle,
+            contactDto.email,
+            contactDto.phoneNumber,
+            contactDto.secondaryPhoneNumber,
+            contactDto.isTermsAndConditionsAccepted);
     }
 }
