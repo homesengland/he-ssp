17bf04b8d0e43dc39eec57fec215294fa1fa90a6
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-09-04 13:10:48 +0200

    tracing added

--- src/HE.CRM.Plugins/Services/LoanApplication/LoanApplicationService.cs
+++ src/HE.CRM.Plugins/Services/LoanApplication/LoanApplicationService.cs
@@ -253,7 +253,7 @@ namespace HE.CRM.Plugins.Services.LoanApplication
         public void CheckIfOwnerCanBeChanged(invln_Loanapplication target, invln_Loanapplication preImage)
         {
             TracingService.Trace($"preImage: value {preImage.StatusCode.Value}, other {preImage.StatusCode}");
-            TracingService.Trace($"preImage: value {target.StatusCode?.Value}, other {target.StatusCode}");
+            TracingService.Trace($"target: value {target.StatusCode?.Value}, other {target.StatusCode}");
             if (((preImage.StatusCode.Value == (int)invln_Loanapplication_StatusCode.Draft && target.StatusCode == null) || (target.StatusCode != null && target.StatusCode.Value == (int)invln_Loanapplication_StatusCode.Draft)) && target.OwnerId.Id != preImage.OwnerId.Id)
             {
                 throw new InvalidPluginExecutionException("Cannot change owner when status is set to draft");
