745da1ce6085c6f1259c6587e5b7df59c6f2e887
Łukasz Ciura <lukasz.ciura@pwc.com>
2023-09-12 12:03:32 +0200

    #77533 Common Authorization for AHP and Loans

--- src/HE.InvestmentLoans.Common/Authorization/AuthorizationExtensions.cs
+++ src/HE.InvestmentLoans.Common/Authorization/AuthorizationExtensions.cs
@@ -1,7 +1,6 @@
 using He.Identity.Auth0;
 using He.Identity.Mvc;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HE.InvestmentLoans.Common.Authorization;
