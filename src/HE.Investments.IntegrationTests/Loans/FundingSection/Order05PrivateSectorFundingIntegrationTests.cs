f7bedd5081973e2abacb154a5693a4f7ec0bb24a
Dawid Pawulski <dawid.pawulski@pwc.com>
2023-09-20 21:20:10 +0200

    Create integration tests

---
+++ src/HE.Investments.IntegrationTests/Loans/FundingSection/Order05PrivateSectorFundingIntegrationTests.cs
@@ -0,0 +1,140 @@
+using System.Diagnostics.CodeAnalysis;
+using AngleSharp.Html.Dom;
+using He.AspNetCore.Mvc.Gds.Components.Constants;
+using HE.InvestmentLoans.Common.Utils.Constants;
+using HE.InvestmentLoans.IntegrationTests.IntegrationFramework;
+using HE.InvestmentLoans.IntegrationTests.IntegrationFramework.Extensions;
+using HE.InvestmentLoans.IntegrationTests.IntegrationFramework.TestData;
+using HE.InvestmentLoans.IntegrationTests.Loans.LoansHelpers.Pages;
+using HE.InvestmentLoans.WWW.Views.FundingV2.Consts;
+using Xunit;
+using Xunit.Extensions.Ordering;
+using FormOptions = HE.InvestmentLoans.Common.Utils.Constants.FormOption;
+
+namespace HE.InvestmentLoans.IntegrationTests.Loans.FundingSection;
+
+[Order(5)]
+[SuppressMessage("xUnit", "xUnit1004", Justification = "Waits for DevOps configuration - #76791")]
+public class Order05PrivateSectorFundingIntegrationTests : IntegrationTest
+{
+    public Order05PrivateSectorFundingIntegrationTests(IntegrationTestFixture<Program> fixture)
+        : base(fixture)
+    {
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(1)]
+    public async Task Order01_ShouldDisplayValidationError_WhenYesIsSelectedAndApplyResultIsNotProvided()
+    {
+        // given
+        var privateSectorFundingPage = await TestClient.NavigateTo(FundingPageUrls.PrivateSectorFunding(GetSharedData<string>(SharedKeys.ApplicationLoanIdInDraftStatusKey)));
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        privateSectorFundingPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.Yes }, { "PrivateSectorFundingResult", string.Empty }, { "PrivateSectorFundingReason", string.Empty } });
+
+        // then
+        privateSectorFundingPage
+            .UrlEndWith(FundingPageUrls.PrivateSectorFundingSuffix)
+            .HasTitle(FundingPageTitles.PrivateSectorFunding)
+            .ContainsValidationMessage(ValidationErrorMessage.EnterMoreDetails);
+
+        SetSharedData(SharedKeys.CurrentPageKey, privateSectorFundingPage);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(2)]
+    public async Task Order02_ShouldDisplayValidationError_WhenYesIsSelectedAndApplyResultIsLongerThan1500Characters()
+    {
+        // given
+        var privateSectorFundingPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        privateSectorFundingPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.Yes }, { "PrivateSectorFundingResult", TextTestData.TextWithLenght1501 }, { "PrivateSectorFundingReason", string.Empty } });
+
+        // then
+        privateSectorFundingPage
+            .UrlEndWith(FundingPageUrls.PrivateSectorFundingSuffix)
+            .HasTitle(FundingPageTitles.PrivateSectorFunding)
+            .ContainsValidationMessage(ValidationErrorMessage.LongInputLengthExceeded(FormOptions.FieldNameForInputLengthValidation.PrivateSectorFundingResult));
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(3)]
+    public async Task Order03_ShouldDisplayValidationError_WhenNoIsSelectedAndNotApplyingReasonIsNotProvided()
+    {
+        // given
+        var privateSectorFundingPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        privateSectorFundingPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.No }, { "PrivateSectorFundingResult", string.Empty }, { "PrivateSectorFundingReason", string.Empty } });
+
+        // then
+        privateSectorFundingPage
+            .UrlEndWith(FundingPageUrls.PrivateSectorFundingSuffix)
+            .HasTitle(FundingPageTitles.PrivateSectorFunding)
+            .ContainsValidationMessage(ValidationErrorMessage.EnterMoreDetails);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(4)]
+    public async Task Order04_ShouldDisplayValidationError_WhenNoIsSelectedAndNotApplyingReasonIsLongerThan1500Characters()
+    {
+        // given
+        var privateSectorFundingPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        privateSectorFundingPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.No }, { "PrivateSectorFundingResult", string.Empty }, { "PrivateSectorFundingReason", TextTestData.TextWithLenght1501 } });
+
+        // then
+        privateSectorFundingPage
+            .UrlEndWith(FundingPageUrls.PrivateSectorFundingSuffix)
+            .HasTitle(FundingPageTitles.PrivateSectorFunding)
+            .ContainsValidationMessage(ValidationErrorMessage.LongInputLengthExceeded(FormOptions.FieldNameForInputLengthValidation.PrivateSectorFundingReason));
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(5)]
+    public async Task Order05_ShouldMoveToRepaymentSystem_WhenNoIsSelectedAndNotApplyingReasonIsProvided()
+    {
+        // given
+        var privateSectorFundingPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        var repaymentSystemPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.No }, { "PrivateSectorFundingResult", string.Empty }, { "PrivateSectorFundingReason", TextTestData.TextWithLenght1000 } });
+
+        // then
+        repaymentSystemPage
+            .UrlEndWith(FundingPageUrls.RepaymentSystemSuffix)
+            .HasTitle(FundingPageTitles.RepaymentSystem);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(6)]
+    public async Task Order06_ShouldMoveToRepaymentSystem_WhenYesIsSelectedAndApplyResultIsProvided()
+    {
+        // given
+        var privateSectorFundingPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        var repaymentSystemPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.Yes }, { "PrivateSectorFundingResult", TextTestData.TextWithLenght1000 }, { "PrivateSectorFundingReason", string.Empty } });
+
+        // then
+        repaymentSystemPage
+            .UrlEndWith(FundingPageUrls.RepaymentSystemSuffix)
+            .HasTitle(FundingPageTitles.RepaymentSystem);
+    }
+}
---
+++ src/HE.Investments.IntegrationTests/Loans/FundingSection/Order08CheckYourAnswersIntegrationTests.cs
@@ -0,0 +1,307 @@
+using System.Diagnostics.CodeAnalysis;
+using AngleSharp.Html.Dom;
+using FluentAssertions;
+using HE.InvestmentLoans.Common.Utils.Constants.FormOption;
+using HE.InvestmentLoans.IntegrationTests.IntegrationFramework;
+using HE.InvestmentLoans.IntegrationTests.IntegrationFramework.Extensions;
+using HE.InvestmentLoans.IntegrationTests.IntegrationFramework.TestData;
+using HE.InvestmentLoans.IntegrationTests.Loans.LoansHelpers;
+using HE.InvestmentLoans.IntegrationTests.Loans.LoansHelpers.Pages;
+using HE.InvestmentLoans.WWW.Views.FundingV2.Consts;
+using Xunit;
+using Xunit.Extensions.Ordering;
+using CommonResponse = HE.InvestmentLoans.Common.Utils.Constants.FormOption.CommonResponse;
+
+namespace HE.InvestmentLoans.IntegrationTests.Loans.FundingSection;
+
+[Order(8)]
+[SuppressMessage("xUnit", "xUnit1004", Justification = "Waits for DevOps configuration - #76791")]
+public class Order08CheckYourAnswersIntegrationTests : IntegrationTest
+{
+    private readonly string _applicationId;
+
+    public Order08CheckYourAnswersIntegrationTests(IntegrationTestFixture<Program> fixture)
+        : base(fixture)
+    {
+        _applicationId = GetSharedData<string>(SharedKeys.ApplicationLoanIdInDraftStatusKey);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(1)]
+    public async Task Order01_ShouldDisplayDataSummary()
+    {
+        // given
+        var checkYourAnswersPage = await TestClient.NavigateTo(FundingPageUrls.CheckYourAnswers(_applicationId));
+
+        // when
+        var fundingSummary = checkYourAnswersPage.GetSummaryListItems();
+
+        // then
+        fundingSummary[FundingFields.GrossDevelopmentValue].Should().Be("12");
+        fundingSummary[FundingFields.EstimatedTotalCosts].Should().Be("999");
+        fundingSummary[FundingFields.AbnormalCosts].Should().Contain(CommonResponse.Yes).And.Contain(TextTestData.TextWithLenght1000);
+        fundingSummary[FundingFields.PrivateSectorFunding].Should().Contain(CommonResponse.Yes).And.Contain(TextTestData.TextWithLenght1000);
+        fundingSummary[FundingFields.RefinanceOrRepay].Should().Contain(FundingFormOption.Refinance).And.Contain(TextTestData.TextWithLenght1000);
+        fundingSummary[FundingFields.AdditionalProjects].Should().Be(CommonResponse.No);
+
+        SetSharedData(SharedKeys.CurrentPageKey, checkYourAnswersPage);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(2)]
+    public async Task Order02_ShouldDisplayValidationError_WhenNoAnswersAreSelected()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var continueButton = checkYourAnswersPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        checkYourAnswersPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "CheckAnswers", string.Empty } });
+
+        // then
+        checkYourAnswersPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasValidationMessages("Select whether you have completed this section");
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(3)]
+    public async Task Order03_ShouldCompletedSection_WhenYesAnswerIsSelected()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var continueButton = checkYourAnswersPage.GetGdsSubmitButtonById("continue-button");
+
+        // when
+        var taskListPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "CheckAnswers", CommonResponse.Yes } });
+
+        // then
+        taskListPage
+            .UrlEndWith(ApplicationPagesUrls.TaskList(_applicationId))
+            .GetTaskListItems()[TaskListFields.ProvideDetailsAboutFunding].Should().Be("Completed");
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(4)]
+    public async Task Order04_ShouldRedirectToGrossDevelopmentValue_WhenGrossDevelopmentValueClicked()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("gross-development-value-change");
+
+        // when
+        var grossDevelopmentValuePage = await TestClient.NavigateTo(changeLink);
+
+        // then
+        grossDevelopmentValuePage
+            .UrlWithoutQueryEndsWith(FundingPageUrls.GrossDevelopmentValueSuffix)
+            .HasTitle(FundingPageTitles.GrossDevelopmentValue);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(5)]
+    public async Task Order05_ShouldGoBackToCheckAnswers_WhenChangingGrossDevelopmentValue()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("gross-development-value-change");
+        var grossDevelopmentValuePage = await TestClient.NavigateTo(changeLink);
+
+        // when
+        var continueButton = grossDevelopmentValuePage.GetGdsSubmitButtonById("continue-button");
+        var returnPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "GrossDevelopmentValue", "12" } });
+
+        // then
+        returnPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasTitle(FundingPageTitles.CheckAnswers);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(6)]
+    public async Task Order06_ShouldRedirectToEstimatedTotalCosts_WhenEstimatedTotalCostsClicked()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("estimated-total-costs-change");
+
+        // when
+        var estimatedTotalCostsPage = await TestClient.NavigateTo(changeLink);
+
+        // then
+        estimatedTotalCostsPage
+            .UrlWithoutQueryEndsWith(FundingPageUrls.EstimatedTotalCostsSuffix)
+            .HasTitle(FundingPageTitles.EstimatedTotalCosts);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(7)]
+    public async Task Order07_ShouldGoBackToCheckAnswers_WhenChangingEstimatedTotalCosts()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("estimated-total-costs-change");
+        var estimatedTotalCostsPage = await TestClient.NavigateTo(changeLink);
+
+        // when
+        var continueButton = estimatedTotalCostsPage.GetGdsSubmitButtonById("continue-button");
+        var returnPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "TotalCosts", "999" } });
+
+        // then
+        returnPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasTitle(FundingPageTitles.CheckAnswers);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(8)]
+    public async Task Order08_ShouldRedirectToAbnormalCosts_WhenAbnormalCostsClicked()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("abnormal-costs-change");
+
+        // when
+        var abnormalCostsPage = await TestClient.NavigateTo(changeLink);
+
+        // then
+        abnormalCostsPage
+            .UrlWithoutQueryEndsWith(FundingPageUrls.AbnormalCostsSuffix)
+            .HasTitle(FundingPageTitles.AbnormalCosts);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(9)]
+    public async Task Order09_ShouldGoBackToCheckAnswers_WhenChangingAbnormalCosts()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("abnormal-costs-change");
+        var abnormalCostsPage = await TestClient.NavigateTo(changeLink);
+
+        // when
+        var continueButton = abnormalCostsPage.GetGdsSubmitButtonById("continue-button");
+        var returnPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "AbnormalCosts", CommonResponse.Yes }, { "AbnormalCostsInfo", TextTestData.TextWithLenght1000 } });
+
+        // then
+        returnPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasTitle(FundingPageTitles.CheckAnswers);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(10)]
+    public async Task Order10_ShouldRedirectToPrivateSectorFunding_WhenPrivateSectorFundingClicked()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("private-sector-funding-change");
+
+        // when
+        var privateSectorFundingPage = await TestClient.NavigateTo(changeLink);
+
+        // then
+        privateSectorFundingPage
+            .UrlWithoutQueryEndsWith(FundingPageUrls.PrivateSectorFundingSuffix)
+            .HasTitle(FundingPageTitles.PrivateSectorFunding);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(11)]
+    public async Task Order11_ShouldGoBackToCheckAnswers_WhenChangingPrivateSectorFunding()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("private-sector-funding-change");
+        var privateSectorFundingPage = await TestClient.NavigateTo(changeLink);
+
+        // when
+        var continueButton = privateSectorFundingPage.GetGdsSubmitButtonById("continue-button");
+        var returnPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "PrivateSectorFunding", CommonResponse.Yes }, { "PrivateSectorFundingResult", TextTestData.TextWithLenght1000 }, { "PrivateSectorFundingReason", string.Empty } });
+
+        // then
+        returnPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasTitle(FundingPageTitles.CheckAnswers);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(12)]
+    public async Task Order12_ShouldRedirectToRepaymentSystem_WhenRepaymentSystemClicked()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("repayment-system-change");
+
+        // when
+        var repaymentSystemPage = await TestClient.NavigateTo(changeLink);
+
+        // then
+        repaymentSystemPage
+            .UrlWithoutQueryEndsWith(FundingPageUrls.RepaymentSystemSuffix)
+            .HasTitle(FundingPageTitles.RepaymentSystem);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(13)]
+    public async Task Order13_ShouldGoBackToCheckAnswers_WhenChangingRepaymentSystem()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("repayment-system-change");
+        var repaymentSystemPage = await TestClient.NavigateTo(changeLink);
+
+        // when
+        var continueButton = repaymentSystemPage.GetGdsSubmitButtonById("continue-button");
+        var returnPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "Refinance", FundingFormOption.Refinance }, { "RefinanceInfo", TextTestData.TextWithLenght1000 } });
+
+        // then
+        returnPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasTitle(FundingPageTitles.CheckAnswers);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(14)]
+    public async Task Order14_ShouldRedirectToAdditionalProjects_WhenAdditionalProjectsClicked()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("additional-projects-change");
+
+        // when
+        var additionalProjectsPage = await TestClient.NavigateTo(changeLink);
+
+        // then
+        additionalProjectsPage
+            .UrlWithoutQueryEndsWith(FundingPageUrls.AdditionalProjectsSuffix)
+            .HasTitle(FundingPageTitles.AdditionalProjects);
+    }
+
+    [Fact(Skip = LoansConfig.SkipTest)]
+    [Order(15)]
+    public async Task Order15_ShouldGoBackToCheckAnswers_WhenChangingAdditionalProjects()
+    {
+        // given
+        var checkYourAnswersPage = GetSharedData<IHtmlDocument>(SharedKeys.CurrentPageKey);
+        var changeLink = checkYourAnswersPage.GetAnchorElementById("additional-projects-change");
+        var additionalProjectsPage = await TestClient.NavigateTo(changeLink);
+
+        // when
+        var continueButton = additionalProjectsPage.GetGdsSubmitButtonById("continue-button");
+        var returnPage = await TestClient.SubmitButton(
+            continueButton, new Dictionary<string, string> { { "AdditionalProjects", CommonResponse.No } });
+
+        // then
+        returnPage
+            .UrlEndWith(FundingPageUrls.CheckYourAnswersSuffix)
+            .HasTitle(FundingPageTitles.CheckAnswers);
+    }
+}
