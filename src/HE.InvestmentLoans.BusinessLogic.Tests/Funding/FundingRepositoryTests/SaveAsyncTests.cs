a37796150d8a9c552dcd070c8045b3bda1ec0ee5
Dawid Pawulski <dawid.pawulski@pwc.com>
2023-09-20 17:42:53 +0200

    SaveAsyncs unit test

--- src/HE.InvestmentLoans.BusinessLogic.Tests/Funding/FundingRepositoryTests/SaveAsyncTests.cs
+++ src/HE.InvestmentLoans.BusinessLogic.Tests/Funding/FundingRepositoryTests/SaveAsyncTests.cs
@@ -36,23 +36,23 @@ public class SaveAsyncTests : TestBase<FundingRepository>
 
         // then
         organizationServiceMock
-            .Verify(
-                x =>
-                    x.ExecuteAsync(
-                        It.Is<invln_updatesingleloanapplicationRequest>(y =>
-                            y.invln_accountid == userAccount.AccountId.ToString() &&
-                            y.invln_loanapplicationid == fundingEntity.LoanApplicationId.ToString() &&
-                            y.invln_loanapplication.Contains($"projectGdv\":{fundingEntity.GrossDevelopmentValue!.Value}") &&
-                            y.invln_loanapplication.Contains($"projectEstimatedTotalCost\":\"{fundingEntity.EstimatedTotalCosts!.Value}\"") &&
-                            y.invln_loanapplication.Contains($"projectAbnormalCosts\":{fundingEntity.AbnormalCosts!.IsAnyAbnormalCost}") &&
-                            y.invln_loanapplication.Contains($"projectAbnormalCostsInformation\":{fundingEntity.AbnormalCosts!.AbnormalCostsAdditionalInformation}") &&
-                            y.invln_loanapplication.Contains($"privateSectorApproach\":{fundingEntity.PrivateSectorFunding!.IsApplied}") &&
-                            y.invln_loanapplication.Contains($"privateSectorApproachInformation\":{fundingEntity.PrivateSectorFunding!.PrivateSectorFundingApplyResult}") &&
-                            y.invln_loanapplication.Contains($"additionalProjects\":{fundingEntity.AdditionalProjects!.IsThereAnyAdditionalProject}") &&
-                            y.invln_loanapplication.Contains($"refinanceRepayment\":{fundingEntity.RepaymentSystem!.Refinance!.Value}") &&
-                            y.invln_loanapplication.Contains($"refinanceRepaymentDetails\":{fundingEntity.RepaymentSystem!.Refinance!.AdditionalInformation}") &&
-                            y.invln_loanapplication.Contains($"FundingDetailsCompletionStatus\":858110001")),
-                        CancellationToken.None),
-                Times.Once);
+    .Verify(
+        x =>
+            x.ExecuteAsync(
+                It.Is<invln_updatesingleloanapplicationRequest>(y =>
+                    y.invln_accountid == userAccount.AccountId.ToString() &&
+                    y.invln_loanapplicationid == fundingEntity.LoanApplicationId.ToString() &&
+                    y.invln_loanapplication.Contains($"projectGdv\":{fundingEntity.GrossDevelopmentValue!.Value}") &&
+                    y.invln_loanapplication.Contains($"projectEstimatedTotalCost\":{fundingEntity.EstimatedTotalCosts!.Value}") &&
+                    y.invln_loanapplication.Contains($"projectAbnormalCosts\":{fundingEntity.AbnormalCosts!.IsAnyAbnormalCost.ToString().ToLowerInvariant()}") &&
+                    y.invln_loanapplication.Contains($"projectAbnormalCostsInformation\":\"{fundingEntity.AbnormalCosts!.AbnormalCostsAdditionalInformation}\"") &&
+                    y.invln_loanapplication.Contains($"privateSectorApproach\":{fundingEntity.PrivateSectorFunding!.IsApplied.ToString().ToLowerInvariant()}") &&
+                    y.invln_loanapplication.Contains($"privateSectorApproachInformation\":\"{fundingEntity.PrivateSectorFunding.PrivateSectorFundingNotApplyingReason}\"") &&
+                    y.invln_loanapplication.Contains($"additionalProjects\":{fundingEntity.AdditionalProjects!.IsThereAnyAdditionalProject.ToString().ToLowerInvariant()}") &&
+                    y.invln_loanapplication.Contains($"refinanceRepayment\":\"{fundingEntity.RepaymentSystem!.Refinance!.Value}\"") &&
+                    y.invln_loanapplication.Contains($"refinanceRepaymentDetails\":\"{fundingEntity.RepaymentSystem!.Refinance!.AdditionalInformation}\"") &&
+                    y.invln_loanapplication.Contains($"FundingDetailsCompletionStatus\":858110001")),
+                CancellationToken.None),
+        Times.Once);
     }
 }
