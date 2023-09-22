030deda614289356b7dc01084446505cf6fccc27
Pawel Koprowski <pawel.koprowski@pwc.com>
2023-09-22 09:49:54 +0200

    Configuration options modified

--- src/HE.InvestmentLoans.WWW/Extensions/ServiceCollectionExtension.cs
+++ src/HE.InvestmentLoans.WWW/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,9 @@
+using HE.InvestmentLoans.Common.Configuration;
 using HE.InvestmentLoans.Common.Models.App;
 using HE.InvestmentLoans.Common.Services;
 using HE.InvestmentLoans.Common.Services.Interfaces;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using StackExchange.Redis;
 
@@ -17,23 +19,25 @@ public static class ServiceCollectionExtension
         services.AddSingleton<ICacheConfig>(x => x.GetRequiredService<IAppConfig>().Cache);
     }
 
-    public static void AddCache(this IServiceCollection services, ICacheConfig config, IAppConfig appConfig)
+    public static void AddCache(this IServiceCollection services, IAppConfig config, IAppConfig appConfig)
     {
-        if (string.IsNullOrEmpty(config.RedisConnectionString) || config.RedisConnectionString == "off")
+        if (string.IsNullOrEmpty(config.Cache.RedisConnectionString) || config.Cache.RedisConnectionString == "off")
         {
             services.AddSingleton<ICacheService, MemoryCacheService>();
         }
         else
         {
-            services.AddSingleton<ICacheService, RedisService>();
+            var redis = new RedisConfigurationOptions(config.Cache);
+
+            services.AddSingleton<ICacheService>(x => new RedisService(config, redis.ConfigurationOptions));
             services.AddDataProtection()
                 .SetApplicationName(appConfig.AppName!)
-                .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect(config.RedisConnectionString), "DataProtection-Keys");
+                .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect(redis.ConfigurationOptions), "DataProtection-Keys");
 
             services.AddStackExchangeRedisCache(action =>
             {
                 action.InstanceName = "redis";
-                action.Configuration = config.RedisConnectionString;
+                action.ConfigurationOptions = redis.ConfigurationOptions;
             });
         }
     }
