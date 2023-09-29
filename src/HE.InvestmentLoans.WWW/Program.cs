79861471e92adff73007f1a032524c990e441a43
Pawel Koprowski <pawel.koprowski@pwc.com>
2023-09-29 08:59:29 +0200

    test

--- src/HE.InvestmentLoans.WWW/Program.cs
+++ src/HE.InvestmentLoans.WWW/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.FeatureManagement;
 #pragma warning disable CA1812
 var builder = WebApplication.CreateBuilder(args);
 #pragma warning restore CA1812
-#pragma warning restore CA1852  
+#pragma warning restore CA1852   
 
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.Configure<AppConfig>(builder.Configuration.GetSection("AppConfiguration"));
