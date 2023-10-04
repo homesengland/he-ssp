c8b84d5ac8703187943438089e05100c6e21de89
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2023-10-04 09:24:11 +0200

    Update Organisation search mockup (#353)

--- src/HE.InvestmentLoans.WWW/Views/Organization/OrganizationPageTitles.cs
+++ src/HE.InvestmentLoans.WWW/Views/Organization/OrganizationPageTitles.cs
@@ -6,7 +6,7 @@ public static class OrganizationPageTitles
 
     public const string ConfirmYourSelection = "Confirm your selection";
 
-    public const string SearchForYourOrganisation = "Search for your organisation";
+    public const string SearchForYourOrganisation = "Your organisation details";
 
     public const string NoMatch = "The details you entered did not match our records";
 }
