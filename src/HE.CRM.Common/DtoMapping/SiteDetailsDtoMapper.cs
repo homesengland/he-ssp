739581bde15e98e33af4eb7992dfec0ddd4ca046
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-10-04 14:05:51 +0200

    new fields to mapping added

--- src/HE.CRM.Common/DtoMapping/SiteDetailsDtoMapper.cs
+++ src/HE.CRM.Common/DtoMapping/SiteDetailsDtoMapper.cs
@@ -35,6 +35,8 @@ namespace HE.CRM.Common.DtoMapping
                 invln_TypeofSite = MapTypeOfSite(siteDetail.typeOfSite),
                 invln_Valuationsource = MapValuationSource(siteDetail.valuationSource),
                 invln_Whoprovided = siteDetail.whoProvided,
+                invln_planningpermissionstatus = siteDetail.planningPermissionStatus,
+                invln_startdate = siteDetail.startDate,
             };
             if (Guid.TryParse(loanApplicationGuid, out Guid applicationId))
             {
@@ -74,6 +76,8 @@ namespace HE.CRM.Common.DtoMapping
                 typeOfSite = MapTypeOfSiteOptionSetToString(siteDetails.invln_TypeofSite),
                 valuationSource = MapValuationSourceOptionSetToString(siteDetails.invln_Valuationsource),
                 whoProvided = siteDetails.invln_Whoprovided,
+                startDate = siteDetails.invln_startdate,
+                planningPermissionStatus = siteDetails.invln_planningpermissionstatus,
             };
             return siteDetailToReturn;
         }
