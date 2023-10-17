12b93170c2762e7172323c0f358a2f2ab1d5f010
Łukasz Ciura <lukasz.ciura@pwc.com>
2023-10-17 16:47:59 +0200

    #79391 Security and Project - routing

--- src/HE.InvestmentLoans.WWW/Controllers/ProjectController.cs
+++ src/HE.InvestmentLoans.WWW/Controllers/ProjectController.cs
@@ -33,15 +33,8 @@ public class ProjectController : WorkflowController<ProjectState>
 
     [HttpGet("start")]
     [WorkflowState(ProjectState.Index)]
-    public async Task<IActionResult> StartProject(Guid id, Guid projectId)
+    public IActionResult StartProject(Guid id, Guid projectId)
     {
-        var response = await _mediator.Send(new GetLoanApplicationQuery(LoanApplicationId.From(id)));
-
-        if (response.LoanApplication.IsReadOnly())
-        {
-            return RedirectToAction("CheckAnswers", new { Id = id, ProjectId = projectId });
-        }
-
         return View("Index", LoanApplicationId.From(id));
     }
 
