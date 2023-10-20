29ca948e66d26621b17825059c7bf7b034d83900
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2023-10-20 10:47:18 +0200

    AB#79078 Update for last modified by views (#448)

--- src/HE.Investments.IntegrationTests/Loans/LoansHelpers/Extensions/TaskListExtensions.cs
+++ src/HE.Investments.IntegrationTests/Loans/LoansHelpers/Extensions/TaskListExtensions.cs
@@ -11,7 +11,7 @@ public static class TaskListExtensions
         var lastSavedLabel = taskListPage.GetElementById("last-saved-label");
         lastSavedLabel.Should().NotBeNull("Last saved data should be visible");
 
-        var dateTimeAsString = lastSavedLabel!.TextContent.Trim().Replace("Last saved on ", string.Empty).Replace("at ", string.Empty);
+        var dateTimeAsString = lastSavedLabel!.TextContent.Replace("Last saved on ", string.Empty).Split("by").First().Trim();
         dateTime = DateTime.Parse(dateTimeAsString, CultureInfo.GetCultureInfo("en-GB"));
         return taskListPage;
     }
