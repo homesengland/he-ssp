85dfd55ed6d5b61ff3fed15f0c47c905fe84c60d
Pawel Koprowski <pawel.koprowski@pwc.com>
2023-10-26 12:34:30 +0200

    Default folders for loan

---
+++ src/HE.InvestmentLoans.BusinessLogic/CompanyStructure/Constants/CompanyStructureConstants.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HE.InvestmentLoans.BusinessLogic.CompanyStructure.Constants;
+internal class CompanyStructureConstants
+{
+    public const string MoreInformationAboutOrganizationExternal = "/more-information-about-organization/external";
+    public const string MoreInformationAboutOrganizationInternal = "/more-information-about-organization/internal";
+}
