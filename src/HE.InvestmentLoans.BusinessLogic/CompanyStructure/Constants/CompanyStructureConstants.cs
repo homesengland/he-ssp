8c5065968bf7d4c036e3401c8392aea5a3c6d0ee
Pawel Koprowski <pawel.koprowski@pwc.com>
2023-10-26 12:38:16 +0200

    fix

--- src/HE.InvestmentLoans.BusinessLogic/CompanyStructure/Constants/CompanyStructureConstants.cs
+++ src/HE.InvestmentLoans.BusinessLogic/CompanyStructure/Constants/CompanyStructureConstants.cs
@@ -7,6 +7,6 @@ using System.Threading.Tasks;
 namespace HE.InvestmentLoans.BusinessLogic.CompanyStructure.Constants;
 internal class CompanyStructureConstants
 {
-    public const string MoreInformationAboutOrganizationExternal = "/more-information-about-organization/external";
-    public const string MoreInformationAboutOrganizationInternal = "/more-information-about-organization/internal";
+    public const string MoreInformationAboutOrganizationExternal = "/external/more-information-about-organization";
+    public const string MoreInformationAboutOrganizationInternal = "/internal/more-information-about-organization";
 }
