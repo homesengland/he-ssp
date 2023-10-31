1a14a3f8d634094f3c72be63711181b7808ed10e
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2023-10-31 15:07:08 +0100

    AB#82280 Update readonly information for Check anwers views (#515)

---
+++ src/HE.InvestmentLoans.WWW/Views/CompanyStructureV2/Consts/CompanyStructurePageTitles.cs
@@ -0,0 +1,7 @@
+namespace HE.InvestmentLoans.WWW.Views.CompanyStructureV2.Consts;
+
+public class CompanyStructurePageTitles
+{
+    public const string CheckAnswers = "Check your answers";
+    public const string ReadOnlyAnswers = "Your answers";
+}
