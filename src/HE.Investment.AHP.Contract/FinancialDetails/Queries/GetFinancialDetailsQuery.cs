02ac9493a274c055c6cafb7819cf9c73d66065e3
Rafał Gajewski <rafal.gajewski@pwc.com>
2023-11-09 14:23:02 +0100

    removed unused usings

--- src/HE.Investment.AHP.Contract/FinancialDetails/Queries/GetFinancialDetailsQuery.cs
+++ src/HE.Investment.AHP.Contract/FinancialDetails/Queries/GetFinancialDetailsQuery.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HE.InvestmentLoans.Common.Utils.Enums;
 using MediatR;
-using ApplicationId = HE.Investment.AHP.Contract.FinancialDetails.ValueObjects.ApplicationId;
 
 namespace HE.Investment.AHP.Contract.FinancialDetails.Queries;
 
