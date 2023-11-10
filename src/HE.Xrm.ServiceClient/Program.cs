0ad27105793aaeb1c3ac004214e470297e901cae
burzynskijakub <133094318+burzynskijakub@users.noreply.github.com>
2023-11-10 09:03:59 +0100

    error fix (#565)

--- src/HE.Xrm.ServiceClient/Program.cs
+++ src/HE.Xrm.ServiceClient/Program.cs
@@ -85,10 +85,15 @@ namespace HE.Xrm.ServiceClientExample
             };
             var fieldsToUpdate = $"{nameof(invln_scheme.invln_Tenure).ToLower()}";
             var app = JsonSerializer.Serialize(application);
-            var req2 = new invln_searchlocalauthorityRequest() //get loan applications related to account and contact with given data
+            var req2 = new invln_getahpapplicationRequest() //get loan applications related to account and contact with given data
             {
+                invln_userid = "1",
+                invln_organisationid = "1",
+                invln_applicationid = "54501650-d77e-ee11-8179-002248004f63"
+                //invln_appfieldstoretrieve = fieldsToUpdate,
             };
-            var resp2 = (invln_searchlocalauthorityResponse)serviceClient.Execute(req2);
+            var resp2 = (invln_getahpapplicationResponse)serviceClient.Execute(req2);
+
             Console.WriteLine("A web service connection was not established.");
         }
 
