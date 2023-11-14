d0dc25bf39cb3398e9d4d9bc454167918ee87c23
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-11-14 11:55:11 +0100

    check if application exists fix

--- src/HE.CRM.AHP.Plugins/Handlers/AHPApplication/CheckIfApplicationWithNewNameExistsHandler.cs
+++ src/HE.CRM.AHP.Plugins/Handlers/AHPApplication/CheckIfApplicationWithNewNameExistsHandler.cs
@@ -9,6 +9,7 @@ namespace HE.CRM.AHP.Plugins.Handlers.AHPApplication
         #region Fields
 
         private invln_scheme target => ExecutionData.Target;
+        private invln_scheme preImage => ExecutionData.Context.PreEntityImages.ContainsKey(nameof(ExecutionData.PreImage)) ? ExecutionData.PreImage : null;
 
         #endregion
         #region Base Methods Overrides
@@ -19,7 +20,7 @@ namespace HE.CRM.AHP.Plugins.Handlers.AHPApplication
 
         public override void DoWork()
         {
-            CrmServicesFactory.Get<IApplicationService>().CheckIfApplicationWithNewNameExists(target);
+            CrmServicesFactory.Get<IApplicationService>().CheckIfApplicationWithNewNameExists(target, preImage);
         }
 
         #endregion
