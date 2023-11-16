fb4d928cb98a363a3d7e5db6a08c8da6dc70cd61
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-11-16 15:47:53 +0100

    creating fix

--- src/HE.CRM.AHP.Plugins/Services/HomeType/HomeTypeService.cs
+++ src/HE.CRM.AHP.Plugins/Services/HomeType/HomeTypeService.cs
@@ -67,32 +67,31 @@ namespace HE.CRM.AHP.Plugins.Services.HomeType
         {
             var homeTypeDto = JsonSerializer.Deserialize<HomeTypeDto>(homeType);
             var homeTypeMapped = HomeTypeMapper.MapDtoToRegularEntity(homeTypeDto);
-            invln_HomeType homeTypeToUpdateOrCreate;
-            if (!string.IsNullOrEmpty(fieldsToSet))
-            {
-                var fields = fieldsToSet.Split(',');
-                homeTypeToUpdateOrCreate = new invln_HomeType();
-                foreach (var field in fields)
-                {
-                    TracingService.Trace($"field name {field}");
-                    if (homeTypeMapped.Contains(field))
-                    {
-                        TracingService.Trace($"contains");
-                        homeTypeToUpdateOrCreate[field] = homeTypeMapped[field];
-                    }
-                }
-            }
-            else
-            {
-                homeTypeToUpdateOrCreate = homeTypeMapped;
-            }
-
             if (string.IsNullOrEmpty(homeTypeDto.id))
             {
-                _ = _homeTypeRepository.Create(homeTypeToUpdateOrCreate);
+                _ = _homeTypeRepository.Create(homeTypeMapped);
             }
             else
             {
+                invln_HomeType homeTypeToUpdateOrCreate;
+                if (!string.IsNullOrEmpty(fieldsToSet))
+                {
+                    var fields = fieldsToSet.Split(',');
+                    homeTypeToUpdateOrCreate = new invln_HomeType();
+                    foreach (var field in fields)
+                    {
+                        TracingService.Trace($"field name {field}");
+                        if (homeTypeMapped.Contains(field))
+                        {
+                            TracingService.Trace($"contains");
+                            homeTypeToUpdateOrCreate[field] = homeTypeMapped[field];
+                        }
+                    }
+                }
+                else
+                {
+                    homeTypeToUpdateOrCreate = homeTypeMapped;
+                }
                 homeTypeToUpdateOrCreate.Id = new Guid(homeTypeDto.id);
                 _homeTypeRepository.Update(homeTypeToUpdateOrCreate);
             }
