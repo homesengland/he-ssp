e67d0865a62cde10cbde34a9a3ddd2ce2d3e5f8f
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2023-11-22 15:35:52 +0100

    AB#84680 merge feature flags (#633)

--- src/HE.Investments.Common/FeatureFlags.cs
+++ src/HE.Investments.Common/FeatureFlags.cs
@@ -2,6 +2,5 @@ namespace HE.Investments.Common;
 
 public static class FeatureFlags
 {
-    public const string OrganisationDashboardAhpProgram = nameof(OrganisationDashboardAhpProgram);
-    public const string AffordableHomesProgramme2021To2026 = nameof(AffordableHomesProgramme2021To2026);
+    public const string AhpProgram = nameof(AhpProgram);
 }
