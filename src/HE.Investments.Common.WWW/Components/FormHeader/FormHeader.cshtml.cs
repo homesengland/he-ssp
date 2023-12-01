c5d0d7e611d86dc0bfd9afba8f67a092495f8b99
Łukasz Ciura <lukasz.ciura@pwc.com>
2023-12-01 14:46:21 +0100

    #85858 Application list view

--- src/HE.Investments.Common.WWW/Components/FormHeader/FormHeader.cshtml.cs
+++ src/HE.Investments.Common.WWW/Components/FormHeader/FormHeader.cshtml.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace HE.Investments.Common.WWW.Components.FormHeader;
 
 public class FormHeader : ViewComponent
 {
-    public IViewComponentResult Invoke(string title, string? caption = null)
+    public IViewComponentResult Invoke(string title, string? caption = null, string? additionalCssClass = null)
     {
-        return View("FormHeader", (Caption: caption, Title: title));
+        return View("FormHeader", (Caption: caption, Title: title, CssClass: additionalCssClass));
     }
 }
---
+++ src/HE.Investments.Common/Messages/GenericMessages.cs
@@ -0,0 +1,6 @@
+namespace HE.Investments.Common.Messages;
+
+public static class GenericMessages
+{
+    public const string NotProvided = "Not provided";
+}
