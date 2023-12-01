d486aed0546ab157d884ba448ed66a1cb706b6cf
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2023-12-01 15:43:26 +0100

    AB#85574 AB#85579 AB#85578 Supported housing information fixes (#692)

--- src/HE.Investment.AHP.Domain/HomeTypes/ValueObjects/MoreInformation.cs
+++ src/HE.Investment.AHP.Domain/HomeTypes/ValueObjects/MoreInformation.cs
@@ -4,8 +4,8 @@ namespace HE.Investment.AHP.Domain.HomeTypes.ValueObjects;
 
 public class MoreInformation : LongText
 {
-    public MoreInformation(string value)
-        : base(value, nameof(MoreInformation), "More information")
+    public MoreInformation(string value, string fieldDisplayName = "More information")
+        : base(value, nameof(MoreInformation), fieldDisplayName)
     {
     }
 }
