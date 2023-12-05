c0ea237693a33717d0aa87bcdc5a2922b3593182
Jakub Burzynski (PL) <jakub.burzynski@pwc.com>
2023-12-05 12:56:04 +0100

    Get All organisation users for given portal type

--- src/HE.Investments.Organisation/CrmRepository/IContactRepository.cs
+++ src/HE.Investments.Organisation/CrmRepository/IContactRepository.cs
@@ -7,4 +7,6 @@ public interface IContactRepository
     Entity? GetContactViaExternalId(IOrganizationServiceAsync2 service, string contactExternalId, string[]? columnSet = null);
 
     Entity? GetContactWithGivenEmailAndExternalId(IOrganizationServiceAsync2 service, string contactEmail, string contactExternalId);
+
+    List<Entity> GetContactsForOrganisation(IOrganizationServiceAsync2 service, Guid organisationId, string portalType);
 }
