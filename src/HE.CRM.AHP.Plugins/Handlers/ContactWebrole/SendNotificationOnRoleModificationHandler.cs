d02654a27dce0ba74239e4b02c71178a781e5df1
Piotr Samborski <piotr.samborski@pwc.com>
2023-12-13 12:59:21 +0100

    Fix notification plugin error

--- src/HE.CRM.AHP.Plugins/Handlers/ContactWebrole/SendNotificationOnRoleModificationHandler.cs
+++ src/HE.CRM.AHP.Plugins/Handlers/ContactWebrole/SendNotificationOnRoleModificationHandler.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using DataverseModel;
 using HE.Base.Plugins.Handlers;
 using HE.CRM.AHP.Plugins.Services.AhpStatusChange;
 using HE.CRM.AHP.Plugins.Services.GovNotifyEmail;
+using Microsoft.Xrm.Sdk;
 
 namespace HE.CRM.AHP.Plugins.Handlers.ContactWebrole
 {
@@ -14,18 +16,28 @@ namespace HE.CRM.AHP.Plugins.Handlers.ContactWebrole
     {
         #region Fields
 
-        private invln_contactwebrole target => ExecutionData.Target;
+
 
         #endregion
         #region Base Methods Overrides
         public override bool CanWork()
         {
-            return target != null;
+            return true;
         }
 
         public override void DoWork()
         {
-            CrmServicesFactory.Get<IGovNotifyEmailService>().SendNotifications_COMMON_CHANGE_EXTERNAL_USER_PERMISSIONS(target.ToEntityReference());
+            if (ExecutionData.Context.MessageName.ToLower() == "delete")
+            {
+                this.TracingService.Trace("Delete");
+                CrmServicesFactory.Get<IGovNotifyEmailService>().SendNotifications_COMMON_CHANGE_EXTERNAL_USER_PERMISSIONS((EntityReference)ExecutionData.Context.InputParameters["Target"]);
+            }
+            else
+            {
+                this.TracingService.Trace("Update or Create");
+                var target = ExecutionData.Target;
+                CrmServicesFactory.Get<IGovNotifyEmailService>().SendNotifications_COMMON_CHANGE_EXTERNAL_USER_PERMISSIONS(target.ToEntityReference());
+            }
         }
 
         #endregion
