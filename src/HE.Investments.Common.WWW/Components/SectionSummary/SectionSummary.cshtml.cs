f9c8d87a5776b730301eb3c56cde32ce8f7f346c
Rafał Gajewski <rafal.gajewski@pwc.com>
2023-12-15 16:08:36 +0100

    Added missing text + fixed error location

--- src/HE.Investments.Common.WWW/Components/SectionSummary/SectionSummary.cshtml.cs
+++ src/HE.Investments.Common.WWW/Components/SectionSummary/SectionSummary.cshtml.cs
@@ -4,8 +4,8 @@ namespace HE.Investments.Common.WWW.Components.SectionSummary;
 
 public class SectionSummary : ViewComponent
 {
-    public IViewComponentResult Invoke(string title, IList<SectionSummaryItemModel>? items)
+    public IViewComponentResult Invoke(string title, IList<SectionSummaryItemModel>? items, string name = "")
     {
-        return View("SectionSummary", (title, items ?? new List<SectionSummaryItemModel>()));
+        return View("SectionSummary", (title, items ?? new List<SectionSummaryItemModel>(), name));
     }
 }
