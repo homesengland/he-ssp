f8b5fe65cab7798b8ba0cb9e24487e223662d062
rafalgajewski <144999129+rafalgajewski@users.noreply.github.com>
2023-12-15 10:06:09 +0100

    AB#86596 - Added error formating in accordance to gds standards (#763)

---
+++ src/HE.Investments.Common.WWW/Helpers/ExtendedSelectListHelper.cs
@@ -0,0 +1,26 @@
+using HE.Investments.Common.Extensions;
+using HE.Investments.Common.WWW.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HE.Investments.Common.WWW.Helpers;
+
+public static class ExtendedSelectListHelper
+{
+    public static IEnumerable<ExtendedSelectListItem> FromEnum<TEnum>()
+        where TEnum : struct, Enum
+    {
+        return EnumExtensions.GetDefinedValues<TEnum>().Select(FromEnum);
+    }
+
+    public static ExtendedSelectListItem FromEnum<TEnum>(TEnum value)
+        where TEnum : struct, Enum
+    {
+        return FromEnum(value, value.GetDescription());
+    }
+
+    public static ExtendedSelectListItem FromEnum<TEnum>(TEnum value, string label)
+        where TEnum : struct, Enum
+    {
+        return new ExtendedSelectListItem(label, value.ToString(), false);
+    }
+}
