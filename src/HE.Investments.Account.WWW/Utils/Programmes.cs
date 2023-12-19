29db328882b322b70a8cabde6042d0aed9f97297
JJ <jakub.jankowski1987@gmail.com>
2023-12-19 14:37:58 +0100

    AB#86483 Disable Start Application button for user with ViewOnly role (#788)

--- src/HE.Investments.Account.WWW/Utils/Programmes.cs
+++ src/HE.Investments.Account.WWW/Utils/Programmes.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel;
 using HE.Investments.Account.Contract.UserOrganisation;
+using HE.Investments.Account.Shared;
 using HE.Investments.Account.WWW.Models.UserOrganisation;
 using HE.Investments.Account.WWW.Routing;
+using NuGet.Common;
 
 namespace HE.Investments.Account.WWW.Utils;
 
@@ -9,33 +11,22 @@ public class Programmes : IProgrammes
 {
     private readonly ProgrammeUrlConfig _programmeUrlConfig;
 
-    private readonly IDictionary<ProgrammeType, ProgrammeModel> _programmes;
+    private readonly AsyncLazy<IDictionary<ProgrammeType, ProgrammeModel>> _programmes;
 
-    public Programmes(ProgrammeUrlConfig programmeUrlConfig)
+    public Programmes(ProgrammeUrlConfig programmeUrlConfig, IAccountAccessContext accountAccessContext)
     {
         _programmeUrlConfig = programmeUrlConfig;
-        _programmes = new Dictionary<ProgrammeType, ProgrammeModel>
+        _programmes = new AsyncLazy<IDictionary<ProgrammeType, ProgrammeModel>>(async () =>
         {
-            {
-                ProgrammeType.Loans, new(
-                    "Levelling up Home Building Fund",
-                    "You can start a new Levelling Up Home Building Fund application here. This will not affect any of your previous applications.",
-                    $"{_programmeUrlConfig.Loans}/application",
-                    $"{_programmeUrlConfig.Loans}/dashboard")
-            },
-            {
-                ProgrammeType.Ahp, new(
-                    "Affordable Homes Programme 2021-2026 Continuous Market Engagement",
-                    "You can start a new Affordable Homes Programme application here. This will not affect any of your previous applications.",
-                    $"{_programmeUrlConfig.Ahp}/application/name",
-                    $"{_programmeUrlConfig.Ahp}/application")
-            },
-        };
+            var canEditApplication = await accountAccessContext.CanEditApplication();
+            return BuildProgrammeModels(canEditApplication);
+        });
     }
 
-    public ProgrammeModel GetProgramme(ProgrammeType programmeType)
+    public async Task<ProgrammeModel> GetProgramme(ProgrammeType programmeType)
     {
-        return _programmes.TryGetValue(programmeType, out var programmeModel)
+        var programmes = await _programmes;
+        return programmes.TryGetValue(programmeType, out var programmeModel)
             ? programmeModel
             : throw new InvalidEnumArgumentException($"Programme for {programmeType} does not exist.");
     }
@@ -53,5 +44,28 @@ public class Programmes : IProgrammes
         }
 
         throw new InvalidEnumArgumentException($"Programme for {programmeType} does not exist.");
+    }
+
+    private IDictionary<ProgrammeType, ProgrammeModel> BuildProgrammeModels(bool canCreateApplication)
+    {
+        return new Dictionary<ProgrammeType, ProgrammeModel>
+        {
+            {
+                ProgrammeType.Loans, new(
+                    "Levelling up Home Building Fund",
+                    "You can start a new Levelling Up Home Building Fund application here. This will not affect any of your previous applications.",
+                    $"{_programmeUrlConfig.Loans}/application",
+                    $"{_programmeUrlConfig.Loans}/dashboard",
+                    canCreateApplication)
+            },
+            {
+                ProgrammeType.Ahp, new(
+                    "Affordable Homes Programme 2021-2026 Continuous Market Engagement",
+                    "You can start a new Affordable Homes Programme application here. This will not affect any of your previous applications.",
+                    $"{_programmeUrlConfig.Ahp}/application/name",
+                    $"{_programmeUrlConfig.Ahp}/application",
+                    canCreateApplication)
+            },
+        };
     }
 }
