97c4368f0c7533933f1567bf0e63124f3c63c92a
Antonio <144206641+Antek012@users.noreply.github.com>
2023-12-28 05:52:49 +0100

    AB#87561 Added TextAreaWithCounter to HomeType pages (#819)

--- src/HE.Investments.Common.WWW/Components/TextAreaInputWithCounter/TextAreaInputWithCounter.cshtml.cs
+++ src/HE.Investments.Common.WWW/Components/TextAreaInputWithCounter/TextAreaInputWithCounter.cshtml.cs
@@ -1,3 +1,4 @@
+using HE.Investments.Common.WWW.Components.Table;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HE.Investments.Common.WWW.Components.TextAreaInputWithCounter;
@@ -7,12 +8,14 @@ public class TextAreaInputWithCounter : ViewComponent
     public IViewComponentResult Invoke(
         string fieldName,
         string? label = null,
+        ComponentViewModel? descriptionComponent = null,
+        bool? isHeadingLabel = null,
         string? hint = null,
         string? value = null,
-        string? cssClass = null,
+        string? inputCssClass = null,
         int? rows = null,
         int? maxLength = null)
     {
-        return View("TextAreaInputWithCounter", (fieldName, label, hint, value, cssClass, rows ?? 5, maxLength ?? 1500));
+        return View("TextAreaInputWithCounter", (fieldName, label, descriptionComponent, hint, value, inputCssClass, isHeadingLabel ?? false, rows ?? 7, maxLength ?? 1500));
     }
 }
