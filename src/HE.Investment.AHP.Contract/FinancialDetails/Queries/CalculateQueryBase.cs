e688bafb35699cdbf0205ab27af5ad04e6963251
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-01-02 19:29:13 +0100

    fix

--- src/HE.Investment.AHP.Contract/FinancialDetails/Queries/CalculateQueryBase.cs
+++ src/HE.Investment.AHP.Contract/FinancialDetails/Queries/CalculateQueryBase.cs
@@ -3,5 +3,4 @@ using MediatR;
 
 namespace HE.Investment.AHP.Contract.FinancialDetails.Queries;
 
-
 public record CalculateQueryBase(Guid ApplicationId) : IRequest<(OperationResult OperationResult, CalculationSum? CalculationSum)>;
