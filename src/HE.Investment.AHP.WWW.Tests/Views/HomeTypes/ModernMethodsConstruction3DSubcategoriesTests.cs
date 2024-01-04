6e46432da6296b3c6101ce06ee79cd802221eef3
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-01-04 13:52:16 +0100

    Add unit tests

---
+++ src/HE.Investment.AHP.WWW.Tests/Views/HomeTypes/ModernMethodsConstruction3DSubcategoriesTests.cs
@@ -0,0 +1,69 @@
+using HE.Investment.AHP.Contract.HomeTypes.Enums;
+using HE.Investment.AHP.WWW.Models.HomeTypes;
+using HE.Investments.Common.WWWTestsFramework.Helpers;
+
+namespace HE.Investment.AHP.WWW.Tests.Views.HomeTypes;
+
+public class ModernMethodsConstruction3DSubcategoriesTests : HomeTypesTestBase
+{
+    private const string ViewPath = "/Views/HomeTypes/ModernMethodsConstruction3DSubcategories.cshtml";
+
+    [Fact]
+    public async Task ShouldRenderViewWithCheckboxes()
+    {
+        // given
+        var model = new ModernMethodsConstructionModel("My application", "My homes");
+
+        // when
+        var document = await RenderHomeTypePage(ViewPath, model);
+
+        // then
+        document
+            .HasElementWithText("span", "My application - My homes")
+            .HasElementWithText("h1", "Category 1")
+            .HasElementWithText("h2", "Which of these sub-categories of 3D primary structural systems are you using?")
+            .HasElementWithText("span", "Select all that apply.")
+            .HasCheckboxes(
+                "ModernMethodsConstruction3DSubcategories",
+                new[]
+                {
+                    "StructuralChassisOnly",
+                    "StructuralChassisAndInternallyFittedOut",
+                    "StructuralChassisInternallyFittedOutAndExternalCladdingOrRoofingCompleted",
+                    "StructuralChassisInternallyFittedOutAndPoddedRoomAssembled",
+                })
+            .HasElementWithText("button", "Save and continue");
+    }
+
+    [Fact]
+    public async Task ShouldRenderViewWithCheckedCheckboxes()
+    {
+        // given
+        var model = new ModernMethodsConstructionModel("My application", "My homes")
+        {
+            ModernMethodsConstruction3DSubcategories = new List<ModernMethodsConstruction3DSubcategoriesType>
+            {
+                ModernMethodsConstruction3DSubcategoriesType.StructuralChassisAndInternallyFittedOut,
+                ModernMethodsConstruction3DSubcategoriesType.StructuralChassisInternallyFittedOutAndPoddedRoomAssembled,
+            },
+        };
+
+        // when
+        var document = await RenderHomeTypePage(ViewPath, model);
+
+        // then
+        document
+            .HasElementWithText("span", "My application - My homes")
+            .HasElementWithText("h1", "Category 1")
+            .HasElementWithText("h2", "Which of these sub-categories of 3D primary structural systems are you using?")
+            .HasElementWithText("span", "Select all that apply.")
+            .HasCheckedCheckboxes(
+                "ModernMethodsConstruction3DSubcategories",
+                new[]
+                {
+                    "StructuralChassisAndInternallyFittedOut",
+                    "StructuralChassisInternallyFittedOutAndPoddedRoomAssembled",
+                })
+            .HasElementWithText("button", "Save and continue");
+    }
+}
