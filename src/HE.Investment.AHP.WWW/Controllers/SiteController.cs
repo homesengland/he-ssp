80eba44e2b8307be4ffcbf96d3fc7b4e360890a9
Carq <lciura.pl@gmail.com>
2024-01-04 11:49:15 +0100

    AB#87742 Site - Landing Page and Name views (#836)

--- src/HE.Investment.AHP.WWW/Controllers/SiteController.cs
+++ src/HE.Investment.AHP.WWW/Controllers/SiteController.cs
@@ -1,5 +1,8 @@
+using HE.Investment.AHP.Contract.Site;
 using HE.Investment.AHP.Contract.Site.Queries;
+using HE.Investment.AHP.WWW.Workflows;
 using HE.Investments.Account.Shared.Authorization.Attributes;
+using HE.Investments.Common.WWW.Routing;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +10,7 @@ namespace HE.Investment.AHP.WWW.Controllers;
 
 [Route("site")]
 [AuthorizeWithCompletedProfile]
-public class SiteController : Controller
+public class SiteController : WorkflowController<SiteWorkflowState>
 {
     private readonly IMediator _mediator;
 
@@ -17,6 +20,7 @@ public class SiteController : Controller
     }
 
     [HttpGet]
+    [WorkflowState(SiteWorkflowState.Index)]
     public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(new GetSiteListQuery(), cancellationToken);
@@ -28,5 +32,44 @@ public class SiteController : Controller
     {
         var response = await _mediator.Send(new GetSiteDetailsQuery(siteId), cancellationToken);
         return View("Details", response);
+    }
+
+    [HttpGet("start")]
+    [WorkflowState(SiteWorkflowState.Start)]
+    public IActionResult Start()
+    {
+        return View("Start");
+    }
+
+    [HttpPost("start")]
+    [WorkflowState(SiteWorkflowState.Start)]
+    public async Task<IActionResult> StartPost()
+    {
+        return await Continue();
+    }
+
+    [HttpGet("name")]
+    [WorkflowState(SiteWorkflowState.Name)]
+    public IActionResult Name()
+    {
+        return View("Name");
+    }
+
+    [HttpPost("name")]
+    [WorkflowState(SiteWorkflowState.Name)]
+    public async Task<IActionResult> NamePost()
+    {
+        return await Continue();
+    }
+
+    [HttpGet("back")]
+    public Task<IActionResult> Back(SiteWorkflowState currentPage, string? siteId)
+    {
+        return Back(currentPage, new { siteId });
+    }
+
+    protected override Task<IStateRouting<SiteWorkflowState>> Routing(SiteWorkflowState currentState, object? routeData = null)
+    {
+        return Task.FromResult<IStateRouting<SiteWorkflowState>>(new SiteWorkflow(currentState));
     }
 }
