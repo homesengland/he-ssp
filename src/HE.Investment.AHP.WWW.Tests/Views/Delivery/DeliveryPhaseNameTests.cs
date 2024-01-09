182758190d9ceedfab2d2f80e9be79935b7073b2
Rafał Gajewski <rafal.gajewski@pwc.com>
2024-01-09 13:47:28 +0100

    view test fix

--- src/HE.Investment.AHP.WWW.Tests/Views/Delivery/DeliveryPhaseNameTests.cs
+++ src/HE.Investment.AHP.WWW.Tests/Views/Delivery/DeliveryPhaseNameTests.cs
@@ -1,8 +1,10 @@
 using AngleSharp.Html.Dom;
+using HE.Investment.AHP.Contract.Delivery;
 using HE.Investment.AHP.WWW.Models.Delivery;
 using HE.Investments.Common.WWWTestsFramework;
 using HE.Investments.Common.WWWTestsFramework.Helpers;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
 
 namespace HE.Investment.AHP.WWW.Tests.Views.Delivery;
 
@@ -10,13 +12,15 @@ public class DeliveryPhaseNameTests : ViewTestBase
 {
     private readonly string _viewPath = "/Views/DeliveryPhase/Name.cshtml";
 
+    private readonly RouteData _routeData = new(new RouteValueDictionary { { "applicationId", "123" }, { "deliveryPhaseId", "321" } });
+
     [Fact]
     public async Task ShouldDisplayView()
     {
         var model = new DeliveryPhaseNameViewModel(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), "TestApp", string.Empty, "Name");
 
         // given & when
-        var document = await Render(_viewPath, model);
+        var document = await Render(_viewPath, model, routeData: _routeData);
 
         // then
         AssertView(document);
@@ -32,7 +36,7 @@ public class DeliveryPhaseNameTests : ViewTestBase
         modelState.AddModelError(nameof(DeliveryPhaseNameViewModel.DeliveryPhaseName), errorMessage);
 
         // when
-        var document = await Render(_viewPath, model, modelStateDictionary: modelState);
+        var document = await Render(_viewPath, model, modelStateDictionary: modelState, routeData: _routeData);
 
         // then
         AssertView(document, errorMessage);
