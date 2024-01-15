410ceb9a18c24ab54cdbfd518161e9571b80559f
Jakub Jankowski <jakub.jankowski1987@gmail.com>
2024-01-15 10:28:48 +0100

    AB#88253 Disabled people home types integration tests.

--- src/HE.Investments.AHP.IntegrationTests/Extensions/AhpHtmlFluentExtensions.cs
+++ src/HE.Investments.AHP.IntegrationTests/Extensions/AhpHtmlFluentExtensions.cs
@@ -1,5 +1,8 @@
+using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using FluentAssertions;
+using HE.Investments.Common.WWW.Extensions;
+using HE.Investments.TestsUtils.Extensions;
 
 namespace HE.Investments.AHP.IntegrationTests.Extensions;
 
@@ -10,6 +13,18 @@ public static class AhpHtmlFluentExtensions
         var referenceNumber = htmlDocument.GetElementById("reference-number");
         referenceNumber.Should().NotBeNull();
         referenceNumber!.TextContent.Should().MatchRegex("ID (\\d{7}$)");
+        return htmlDocument;
+    }
+
+    public static IHtmlDocument HasChangeAnswerSummaryButton(this IHtmlDocument htmlDocument, string summaryName, out IHtmlAnchorElement changeAnswerButton)
+    {
+        var summaryRow = htmlDocument.GetElementByTestId($"{summaryName.ToIdTag()}-summary");
+        var changeLink = summaryRow.FindDescendant<IHtmlAnchorElement>();
+        changeLink.Should().NotBeNull();
+
+        changeLink!.Text.Trim().Should().Be("Change");
+        changeAnswerButton = changeLink!;
+
         return htmlDocument;
     }
 }
