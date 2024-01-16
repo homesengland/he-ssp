ca89823105566c5fa9c80e7d392b35892d5659ee
Rafał Gajewski <rafal.gajewski@pwc.com>
2024-01-16 01:07:23 +0100

    updated tenure descriptions

--- src/HE.Investment.AHP.WWW/Models/Application/Tenures.cs
+++ src/HE.Investment.AHP.WWW/Models/Application/Tenures.cs
@@ -7,16 +7,16 @@ public static class Tenures
     public static IDictionary<Tenure, string> AvailableTenures => new Dictionary<Tenure, string>
     {
         {
-            Tenure.AffordableRent, "Homes let to working households at a lower cost to give them the opportunity to save for a deposit to but their first home."
+            Tenure.AffordableRent, "Homes let at below market rent by a registered provider. The rent, including service charge, is set at up to 80% of the local market rent for an equivalent home."
         },
         {
             Tenure.SocialRent,
-            "Social rent is low-cost rental social housing that is let at below market rent by a registered provider based on a formula set by the government."
+            "Low-cost rental social housing that is let at below market rent by a registered provider based on a formula set by the government."
         },
-        { Tenure.SharedOwnership, "Shared Ownership allows a customer to purchase a share of a home and pay rent on the remaining share." },
+        { Tenure.SharedOwnership, "Allows a customer to purchase a share of a home and pay rent on the remaining share." },
         {
             Tenure.RentToBuy,
-            "Homes let at below market rent by a registered provider. The rent, including service charge, is set at up to 80% of the local market rent for an equivalent home."
+            "Homes let to working households at a lower cost to give them the opportunity to save for a deposit to buy their first home."
         },
         {
             Tenure.HomeOwnershipLongTermDisabilities,
