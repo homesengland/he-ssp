8100bb6d7c3179f2190200ce98b044127a6784af
Antonio <144206641+Antek012@users.noreply.github.com>
2024-01-18 13:43:30 +0100

    AB#88114 Change renamed to Manage (#908)

--- src/HE.Investments.Account.WWW/Views/Shared/Components/UsersTable/UsersTable.cshtml.cs
+++ src/HE.Investments.Account.WWW/Views/Shared/Components/UsersTable/UsersTable.cshtml.cs
@@ -1,14 +1,65 @@
 using HE.Investments.Account.Contract.Users;
+using HE.Investments.Account.Shared;
+using HE.Investments.Account.WWW.Controllers;
 using HE.Investments.Common.Contract;
 using HE.Investments.Common.Contract.Pagination;
+using HE.Investments.Common.Extensions;
+using HE.Investments.Common.WWW.Components;
+using HE.Investments.Common.WWW.Components.Link;
+using HE.Investments.Common.WWW.Components.Table;
+using HE.Investments.Common.WWW.Helpers;
+using HE.Investments.Common.WWW.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HE.Investments.Account.WWW.Views.Shared.Components.UsersTable;
 
 public class UsersTable : ViewComponent
 {
-    public IViewComponentResult Invoke(PaginationResult<UserDetails> users)
+    private readonly IAccountAccessContext _accountAccessContext;
+
+    public UsersTable(IAccountAccessContext accountAccessContext)
     {
-        return View("UsersTable", users);
+        _accountAccessContext = accountAccessContext;
     }
+
+    public async Task<IViewComponentResult> InvokeAsync(PaginationResult<UserDetails> users)
+    {
+        var canManageUsers = await _accountAccessContext.CanManageUsers();
+        var tableHeaders = new List<TableHeaderViewModel>
+        {
+            new("First name"),
+            new("Last name"),
+            new("Email"),
+            new("Job title"),
+            new("Role"),
+            new("Last active"),
+            new("Remove", IsHidden: true, IsDisplayed: canManageUsers),
+            new("Manage", IsHidden: true, IsDisplayed: canManageUsers),
+        };
+
+        var usersDetails = users.Items.Select(u =>
+            {
+                var tableItems = new List<TableValueViewModel>
+                {
+                    new(u.FirstName),
+                    new(u.LastName),
+                    new(u.Email),
+                    new(u.JobTitle),
+                    new(u.Role?.GetDescription()),
+                    new(u.LastActiveAt == null ? "-" : DateHelper.DisplayAsUkFormatDateTime(u.LastActiveAt)),
+                    new(Component: CreateUsersLinkComponent(u.Id.Value, "Remove", nameof(UsersController.ConfirmUnlink)), IsDisplayed: canManageUsers),
+                    new(Component: CreateUsersLinkComponent(u.Id.Value, "Manage", nameof(UsersController.Manage)), IsDisplayed: canManageUsers),
+                };
+
+                return new TableRowViewModel(tableItems);
+            })
+            .ToList();
+
+        var rows = new PaginationResult<TableRowViewModel>(usersDetails, users.CurrentPage, users.ItemsPerPage, users.TotalItems);
+
+        return View("UsersTable", (tableHeaders, rows));
+    }
+
+    private ComponentViewModel CreateUsersLinkComponent(string id, string text, string action) =>
+        new(nameof(Link), new { text, action, controller = new ControllerName(nameof(UsersController)).WithoutPrefix(), values = new { id } });
 }
