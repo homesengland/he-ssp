89a6221110021203416e99431668448254542dd8
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-01-19 11:59:58 +0100

    fix for delivery workflow

--- src/HE.Investment.AHP.WWW/Workflows/DeliveryPhaseWorkflow.cs
+++ src/HE.Investment.AHP.WWW/Workflows/DeliveryPhaseWorkflow.cs
@@ -44,6 +44,7 @@ public class DeliveryPhaseWorkflow : IStateRouting<DeliveryPhaseWorkflowState>
             return targetState;
         }
 
+#pragma warning disable S2589 // Boolean expressions should not be gratuitous
         return _model switch
         {
             { TypeOfHomes: var x } when x.IsNotProvided() => DeliveryPhaseWorkflowState.TypeOfHomes,
@@ -56,6 +57,7 @@ public class DeliveryPhaseWorkflow : IStateRouting<DeliveryPhaseWorkflowState>
             { } when _model.IsAdditionalPaymentRequested.IsNotProvided() && IsUnregisteredBody() => DeliveryPhaseWorkflowState.UnregisteredBodyFollowUp,
             _ => DeliveryPhaseWorkflowState.CheckAnswers,
         };
+#pragma warning restore S2589 // Boolean expressions should not be gratuitous
     }
 
     public bool CanBeAccessed(DeliveryPhaseWorkflowState state)
