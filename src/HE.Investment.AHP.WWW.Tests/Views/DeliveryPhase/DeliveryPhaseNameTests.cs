55690785e0d8357d620556baed521b4d1cb08d1b
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-01-29 16:54:16 +0100

    test fixes

--- src/HE.Investment.AHP.WWW.Tests/Views/DeliveryPhase/DeliveryPhaseNameTests.cs
+++ src/HE.Investment.AHP.WWW.Tests/Views/DeliveryPhase/DeliveryPhaseNameTests.cs
@@ -46,7 +46,7 @@ public class DeliveryPhaseNameTests : ViewTestBase
         document
             .HasElementWithText("label", "Name your delivery phase")
             .HasElementWithText("div", "Each delivery phase needs a unique name.")
-            .HasGdsSaveAndContinueButton()
+            .HasContinueButton()
             .HasSummaryErrorMessage(nameof(DeliveryPhaseNameViewModel.DeliveryPhaseName), errorMessage, !string.IsNullOrEmpty(errorMessage))
             .HasErrorMessage(nameof(DeliveryPhaseNameViewModel.DeliveryPhaseName), errorMessage, !string.IsNullOrEmpty(errorMessage));
     }
