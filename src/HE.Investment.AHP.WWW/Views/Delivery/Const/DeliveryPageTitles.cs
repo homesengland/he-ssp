9bab194c14f137de13a3a6e1baccc7ed7f205458
Łukasz Ciura <lukasz.ciura@pwc.com>
2024-01-31 09:34:54 +0100

    AB#89635 Build activity type change header

--- src/HE.Investment.AHP.WWW/Views/Delivery/Const/DeliveryPageTitles.cs
+++ src/HE.Investment.AHP.WWW/Views/Delivery/Const/DeliveryPageTitles.cs
@@ -8,7 +8,7 @@ public static class DeliveryPageTitles
 
     public const string Details = "Delivery phase details";
 
-    public const string BuildActivityType = "Build activity type";
+    public const string BuildActivityType = "What is the build activity type?";
 
     public const string ReconfiguringExisting = "Are you reconfiguring existing residential properties to increase the number of homes?";
 
