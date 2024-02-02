ca48e2c29824a86cc8dc1f1ec34ddc1028d6b33f
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-02-02 09:06:57 +0100

    fix DomainModule

--- src/HE.Investment.AHP.Domain/Config/DomainModule.cs
+++ src/HE.Investment.AHP.Domain/Config/DomainModule.cs
@@ -84,9 +84,9 @@ public static class DomainModule
 
     private static void AddApplication(IServiceCollection services)
     {
-        services.AddScoped<IApplicationRepository, ApplicationRepository>();
         services.AddScoped<IAhpProgrammeRepository, AhpProgrammeRepository>();
-        services.AddScoped<IChangeApplicationStatus, ApplicationRepository>();
+        services.AddScoped<IApplicationRepository, ApplicationRepository>();
+        services.AddScoped<IChangeApplicationStatus>(x => x.GetRequiredService<IApplicationRepository>());
     }
 
     private static void AddScheme(IServiceCollection services)
