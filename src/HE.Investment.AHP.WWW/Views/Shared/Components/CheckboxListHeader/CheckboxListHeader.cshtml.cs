6040f5f94b4cd3cfb46c9e4404bd19da684df618
Rafał Gajewski <rafal.gajewski@pwc.com>
2024-02-05 13:48:08 +0100

    used input list header in checkbox list header

--- src/HE.Investment.AHP.WWW/Views/Shared/Components/CheckboxListHeader/CheckboxListHeader.cshtml.cs
+++ src/HE.Investment.AHP.WWW/Views/Shared/Components/CheckboxListHeader/CheckboxListHeader.cshtml.cs
@@ -8,10 +8,12 @@ namespace HE.Investment.AHP.WWW.Views.Shared.Components.CheckboxListHeader;
 public class CheckboxListHeader : ViewComponent
 {
     public IViewComponentResult Invoke(
-        DynamicComponentViewModel headerComponent,
-        DynamicComponentViewModel? detailsComponent = null,
-        string? hint = null)
+        DynamicComponentViewModel? headerComponent = null,
+        string? header = null,
+        string? title = null,
+        string? hint = null,
+        DynamicComponentViewModel? detailsComponent = null)
     {
-        return View("CheckboxListHeader", (headerComponent, detailsComponent, hint));
+        return View("CheckboxListHeader", (headerComponent, header, title, hint, detailsComponent));
     }
 }
