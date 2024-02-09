9abbae8c6c3808e3a0c57abcb0da97515b75e52d
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-02-09 10:52:00 +0100

    Add unit tests

--- src/HE.Investment.AHP.Domain.Tests/Site/TestDataBuilders/SiteEntityBuilder.cs
+++ src/HE.Investment.AHP.Domain.Tests/Site/TestDataBuilders/SiteEntityBuilder.cs
@@ -1,3 +1,4 @@
+using HE.Investment.AHP.Contract.Site.Enums;
 using HE.Investment.AHP.Domain.Site.Entities;
 using HE.Investment.AHP.Domain.Site.ValueObjects;
 using HE.Investments.TestsUtils;
@@ -21,6 +22,26 @@ public class SiteEntityBuilder
             _item,
             nameof(_item.Name),
             new SiteName(name));
+
+        return this;
+    }
+
+    public SiteEntityBuilder WithBuildingForHealthyLife(BuildingForHealthyLifeType buildingForHealthyLife)
+    {
+        PrivatePropertySetter.SetPropertyWithNoSetter(
+            _item,
+            nameof(_item.BuildingForHealthyLife),
+            buildingForHealthyLife);
+
+        return this;
+    }
+
+    public SiteEntityBuilder WithNumberOfGreenLights(NumberOfGreenLights? numberOfGreenLights)
+    {
+        PrivatePropertySetter.SetPropertyWithNoSetter(
+            _item,
+            nameof(_item.NumberOfGreenLights),
+            numberOfGreenLights);
 
         return this;
     }
