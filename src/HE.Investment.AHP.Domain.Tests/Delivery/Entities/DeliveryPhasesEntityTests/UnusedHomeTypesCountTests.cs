3763a9ca762b9ce6a00652703aaf57ae270e7032
JJ <jakub.jankowski1987@gmail.com>
2024-02-12 13:18:43 +0100

    AB#90515 Adjust Delivery list page to new mocks. (#1037)

--- src/HE.Investment.AHP.Domain.Tests/Delivery/Entities/DeliveryPhasesEntityTests/UnusedHomeTypesCountTests.cs
+++ src/HE.Investment.AHP.Domain.Tests/Delivery/Entities/DeliveryPhasesEntityTests/UnusedHomeTypesCountTests.cs
@@ -19,6 +19,27 @@ public class UnusedHomeTypesCountTests
     }
 
     [Fact]
+    public void ShouldReturnNegativeNumber_WhenTooManyHomeTypesAreProvided()
+    {
+        // given
+        var deliveryPhase = new DeliveryPhaseEntityBuilder()
+            .WithHomesToBeDelivered(2, "ht-1")
+            .WithHomesToBeDelivered(2, "ht-2")
+            .Build();
+        var testCandidate = new DeliveryPhasesEntityBuilder()
+            .WithHomesToDeliver(new HomesToDeliverBuilder().WithHomeTypeId("ht-1").WithTotalHomes(1).Build())
+            .WithHomesToDeliver(new HomesToDeliverBuilder().WithHomeTypeId("ht-2").WithTotalHomes(1).Build())
+            .WithDeliveryPhase(deliveryPhase)
+            .Build();
+
+        // when
+        var result = testCandidate.UnusedHomeTypesCount;
+
+        // then
+        result.Should().Be(-2);
+    }
+
+    [Fact]
     public void ShouldReturnTotalHomesToDeliver_WhenThereAreNoDeliveryPhases()
     {
         // given
