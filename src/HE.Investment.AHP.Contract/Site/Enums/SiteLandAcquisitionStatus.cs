05932c4cfdb1edee7c91efbe8325c0ecfe043a3a
rafalgajewski <144999129+rafalgajewski@users.noreply.github.com>
2024-02-12 11:00:37 +0100

    AB#89379 Added workflow changes for land acquisition status (#1036)

---
+++ src/HE.Investment.AHP.Contract/Site/Enums/SiteLandAcquisitionStatus.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel;
+
+namespace HE.Investment.AHP.Contract.Site.Enums;
+
+public enum SiteLandAcquisitionStatus
+{
+    Undefined = 0,
+
+    [Description("Full unconditional ownership, acquisition of freehold or long leasehold interest has occurred")]
+    FullOwnership,
+
+    [Description("Land being gifted or provided at a discount by the local authority")]
+    LandGifted,
+
+    [Description("Conditional acquisition, land option or heads of terms")]
+    ConditionalAcquisition,
+
+    [Description("Land purchase negotiations underway")]
+    PurchaseNegotiationInProgress,
+
+    [Description("Land identified but purchase negotiations not yet started")]
+    PurchaseNegotiationsNotStarted,
+}
