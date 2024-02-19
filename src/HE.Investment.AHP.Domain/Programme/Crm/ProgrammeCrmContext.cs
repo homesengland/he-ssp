88e869d24bc981a0a870608e76eac6f4d48c7f95
Carq <lciura.pl@gmail.com>
2024-02-19 12:06:34 +0100

    AB#88198 Fetch programme (#1074)

--- src/HE.Investment.AHP.Domain/Programme/Crm/ProgrammeCrmContext.cs
+++ src/HE.Investment.AHP.Domain/Programme/Crm/ProgrammeCrmContext.cs
@@ -1,28 +1,40 @@
 extern alias Org;
 
+using HE.Investments.Common.CRM.Model;
+using HE.Investments.Common.CRM.Services;
 using Org::HE.Common.IntegrationModel.PortalIntegrationModel;
 
 namespace HE.Investment.AHP.Domain.Programme.Crm;
 
 public class ProgrammeCrmContext : IProgrammeCrmContext
 {
-    public ProgrammeCrmContext()
+    private readonly ICrmService _service;
+
+    public ProgrammeCrmContext(ICrmService service)
     {
+        _service = service;
     }
 
     public async Task<ProgrammeDto> GetProgramme(string programmeId, CancellationToken cancellationToken)
     {
-        return await Task.FromResult(new ProgrammeDto
+        var request = new invln_getsingleprogrammeRequest
         {
-            name = "AHP",
-            startOn = new DateTime(2021, 6, 10, 0, 0, 0, DateTimeKind.Utc),
-            endOn = new DateTime(2024, 5, 10, 0, 0, 0, DateTimeKind.Utc),
-            milestoneFrameworkItem = new List<MilestoneFrameworkItemDto>
-            {
-                new() { milestone = 1, name = "Acquisition", percentPaid = 0.5m },
-                new() { milestone = 2, name = "StartOnSite", percentPaid = 0.4m },
-                new() { milestone = 3, name = "Completion", percentPaid = 0.1m },
-            },
-        });
+            invln_programmeId = programmeId,
+        };
+
+        var response = await _service.ExecuteAsync<invln_getsingleprogrammeRequest, invln_getsingleprogrammeResponse, ProgrammeDto>(
+            request,
+            r => r.invln_programme,
+            cancellationToken);
+
+        // TODO: #88889 Use Milestone framework from CRM
+        response.milestoneFrameworkItem = new List<MilestoneFrameworkItemDto>
+        {
+            new() { milestone = 1, name = "Acquisition", percentPaid = 0.5m },
+            new() { milestone = 2, name = "StartOnSite", percentPaid = 0.4m },
+            new() { milestone = 3, name = "Completion", percentPaid = 0.1m },
+        };
+
+        return response;
     }
 }
