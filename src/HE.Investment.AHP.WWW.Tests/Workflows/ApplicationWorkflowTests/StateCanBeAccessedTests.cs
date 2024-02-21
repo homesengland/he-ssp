df783c8d03113ac4533f1b07cfdb028f91b9940b
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-02-21 19:36:40 +0100

    Add workflow tests

--- src/HE.Investment.AHP.WWW.Tests/Workflows/ApplicationWorkflowTests/StateCanBeAccessedTests.cs
+++ src/HE.Investment.AHP.WWW.Tests/Workflows/ApplicationWorkflowTests/StateCanBeAccessedTests.cs
@@ -16,6 +16,8 @@ public class StateCanBeAccessedTests
     [InlineData(ApplicationStatus.Draft, ApplicationWorkflowState.Withdraw, true)]
     [InlineData(ApplicationStatus.Draft, ApplicationWorkflowState.CheckAnswers, false)]
     [InlineData(ApplicationStatus.ApplicationSubmitted, ApplicationWorkflowState.CheckAnswers, false)]
+    [InlineData(ApplicationStatus.ApplicationSubmitted, ApplicationWorkflowState.Submit, false)]
+    [InlineData(ApplicationStatus.ApplicationSubmitted, ApplicationWorkflowState.Completed, false)]
     [InlineData(ApplicationStatus.ApplicationSubmitted, ApplicationWorkflowState.Withdraw, true)]
     [InlineData(ApplicationStatus.ApplicationSubmitted, ApplicationWorkflowState.OnHold, true)]
     [InlineData(ApplicationStatus.ApplicationSubmitted, ApplicationWorkflowState.Reactivate, false)]
@@ -56,6 +58,8 @@ public class StateCanBeAccessedTests
     [InlineData(ApplicationWorkflowState.RequestToEdit, false)]
     [InlineData(ApplicationWorkflowState.Withdraw, true)]
     [InlineData(ApplicationWorkflowState.CheckAnswers, true)]
+    [InlineData(ApplicationWorkflowState.Submit, true)]
+    [InlineData(ApplicationWorkflowState.Completed, true)]
     public async Task ShouldReturnValue_WhenApplicationIsCompletelyFilled(ApplicationWorkflowState state, bool expectedResult)
     {
         // given
