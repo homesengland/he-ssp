ecadbbaff560ea5ed4aa3acbf45da773319661d8
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-02-27 11:16:00 +0100

    update organisation structure view

---
+++ src/HE.Investments.Loans.WWW/wwwroot/javascripts/init-gds-modules.js
@@ -0,0 +1,9 @@
+(() => {
+    const initModule = (Module, selector) => {
+        var components = document.querySelectorAll(`[data-module="${selector}"]`)
+        components.forEach(item => new Module(item).init())
+    }
+
+  initModule(window.GOVUKFrontend.CharacterCount, "govuk-character-count");
+
+})();
