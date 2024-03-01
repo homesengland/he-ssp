b2a10721bd3e0431973b08d8836478954e785a77
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2024-03-01 08:23:32 +0100

    AB#91650 Add telephone validation Part I (#1113)

--- src/HE.Investments.Account.Shared/User/ValueObjects/TelephoneNumber.cs
+++ src/HE.Investments.Account.Shared/User/ValueObjects/TelephoneNumber.cs
@@ -1,11 +1,50 @@
-using HE.Investments.Common.Messages;
+using HE.Investments.Common.Contract.Validators;
+using HE.Investments.Common.Domain;
+using HE.Investments.Common.Extensions;
+using HE.Investments.Common.Validators;
 
 namespace HE.Investments.Account.Shared.User.ValueObjects;
 
-public class TelephoneNumber : RequiredStringValueObject
+public class TelephoneNumber : ValueObject
 {
-    public TelephoneNumber(string? value)
-        : base(value, nameof(TelephoneNumber), "your preferred telephone number", MaximumInputLength.ShortInput)
+    public TelephoneNumber(string value)
     {
+        Value = value;
+    }
+
+    private TelephoneNumber(string? telephoneNumber, string fieldName, string fieldLabel, bool isOptional)
+    {
+        Build(telephoneNumber, fieldName, fieldLabel, isOptional).CheckErrors();
+    }
+
+    public string Value { get; private set; }
+
+    public static TelephoneNumber FromString(string? telephoneNumber, string fieldName, string fieldLabel, bool isOptional = false) =>
+        new(telephoneNumber, fieldName, fieldLabel, isOptional);
+
+    public override string ToString()
+    {
+        return Value;
+    }
+
+    protected override IEnumerable<object?> GetAtomicValues()
+    {
+        yield return Value;
+    }
+
+    private OperationResult Build(string? telephoneNumber, string fieldName, string fieldLabel, bool isOptional)
+    {
+        var operationResult = OperationResult.New();
+
+        var value = TelephoneNumberValidator
+            .For(telephoneNumber, fieldName, fieldLabel, isOptional, operationResult)
+            .IsValid();
+
+        if (value.IsProvided())
+        {
+            Value = value!;
+        }
+
+        return operationResult;
     }
 }
---
+++ src/HE.Investments.Common.Tests/Validators/TelephoneNumberValidatorTests/IsValidTests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using HE.Investments.Common.Contract.Validators;
+using HE.Investments.Common.Validators;
+using Xunit;
+
+namespace HE.Investments.Common.Tests.Validators.TelephoneNumberValidatorTests;
+
+public class IsValidTests
+{
+    [Theory]
+    [InlineData("+44 (0)20 7450 4000")]
+    [InlineData("020 7450 4000")]
+    [InlineData("01273 800 900")]
+    [InlineData("07771 900 900")]
+    public void ShouldNotReturnError_WhenValueIsValid(string telephoneNumber)
+    {
+        // given
+        var result = OperationResult.New();
+
+        // when
+        string? validatedTelephoneNumber = TelephoneNumberValidator
+            .For(telephoneNumber, "TelephoneNumber", "test", false, result)
+            .IsValid();
+
+        // then
+        result.HasValidationErrors.Should().BeFalse();
+        validatedTelephoneNumber.Should().NotBeNull();
+        validatedTelephoneNumber.Should().Be(telephoneNumber);
+    }
+
+    [Theory]
+    [InlineData("test")]
+    [InlineData(null)]
+    [InlineData("020 7450 40005")]
+    [InlineData("+444 (0)20 7450 4000")]
+    [InlineData("123 123 123 123")]
+    public void ShouldReturnError_WhenValueIsInvalid(string? telephoneNumber)
+    {
+        // given
+        var result = OperationResult.New();
+
+        // when
+        TelephoneNumberValidator
+            .For(telephoneNumber, "TelephoneNumber", "test", false, result)
+            .IsValid();
+
+        // then
+        result.HasValidationErrors.Should().BeTrue();
+    }
+}
