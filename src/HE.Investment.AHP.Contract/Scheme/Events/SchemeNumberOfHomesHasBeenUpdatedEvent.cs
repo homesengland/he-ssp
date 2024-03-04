9552174e84c263a6530a5be1b7b1a169f5a40839
rafalgajewski <144999129+rafalgajewski@users.noreply.github.com>
2024-03-04 09:57:28 +0100

    AB#91041 - Uncomplete Home Type (#1114)

---
+++ src/HE.Investment.AHP.Contract/Scheme/Events/SchemeNumberOfHomesHasBeenUpdatedEvent.cs
@@ -0,0 +1,6 @@
+﻿using HE.Investment.AHP.Contract.Application;
+using HE.Investments.Common.Contract.Infrastructure.Events;
+
+namespace HE.Investment.AHP.Contract.Scheme.Events;
+
+public record SchemeNumberOfHomesHasBeenUpdatedEvent(AhpApplicationId ApplicationId) : IDomainEvent;
