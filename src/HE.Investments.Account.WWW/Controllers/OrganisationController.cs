21f30241d915c9c39de6f8138d3f8ea1c6a605fb
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-03-04 22:34:25 +0100

    update

--- src/HE.Investments.Account.WWW/Controllers/OrganisationController.cs
+++ src/HE.Investments.Account.WWW/Controllers/OrganisationController.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace HE.Investments.Account.WWW.Controllers;
 
 [Route(OrganisationAccountEndpoints.Controller)]
+[AuthorizeWithCompletedProfile]
 public class OrganisationController : Controller
 {
     private readonly IMediator _mediator;
