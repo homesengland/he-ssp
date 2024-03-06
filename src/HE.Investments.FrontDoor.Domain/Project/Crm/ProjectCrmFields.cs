c3bfdcee7450817ffa6cc17c58e500d28fa5bf9a
Łukasz Ciura <lukasz.ciura@pwc.com>
2024-03-06 12:53:15 +0100

    AB#91801 Support Activities - Update CRM context

---
+++ src/HE.Investments.FrontDoor.Domain/Project/Crm/ProjectCrmFields.cs
@@ -0,0 +1,30 @@
+using System.Collections.Immutable;
+using HE.Investments.Common.CRM.Model;
+using HE.Investments.Common.Extensions;
+
+namespace HE.Investments.FrontDoor.Domain.Project.Crm;
+
+public static class ProjectCrmFields
+{
+    public static readonly IReadOnlyList<string> ProjectToUpdate = new List<string>
+    {
+        nameof(invln_FrontDoorProjectPOC.invln_Name), nameof(invln_FrontDoorProjectPOC.invln_FrontDoorProjectPOCId),
+    };
+
+    public static readonly IReadOnlyList<string> ProjectToRead = ProjectToUpdate.ToList()
+        .Append(new List<string>
+        {
+            nameof(invln_FrontDoorProjectPOC.invln_identifiedsiteName),
+            nameof(invln_FrontDoorProjectPOC.invln_ActivitiesinThisProject),
+            nameof(invln_FrontDoorProjectPOC.invln_activitiesinthisprojectName),
+            nameof(invln_FrontDoorProjectPOC.invln_ProjectSupportsHousingDeliveryinEngland),
+            nameof(invln_FrontDoorProjectPOC.invln_AmountofAffordableHomes),
+            nameof(invln_FrontDoorProjectPOC.invln_IdentifiedSite),
+        })
+        .ToImmutableList();
+
+    public static string FormatFields(this IReadOnlyList<string> fieldsToRetrieve)
+    {
+        return string.Join(",", fieldsToRetrieve.Select(f => f.ToLowerInvariant()));
+    }
+}
