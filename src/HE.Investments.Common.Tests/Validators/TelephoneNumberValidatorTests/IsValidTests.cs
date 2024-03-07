7c9dd12e4ba4c8e114577473ee3c5a7789d9dd98
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-03-07 15:10:57 +0100

    Telephone number validation fix

--- src/HE.Investments.Common.Tests/Validators/TelephoneNumberValidatorTests/IsValidTests.cs
+++ src/HE.Investments.Common.Tests/Validators/TelephoneNumberValidatorTests/IsValidTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using HE.Investments.Common.Contract.Validators;
+using HE.Investments.Common.Messages;
 using HE.Investments.Common.Validators;
 using Xunit;
 
@@ -30,16 +31,12 @@ public class IsValidTests
     }
 
     [Theory]
-    [InlineData("test")]
-    [InlineData(null)]
-    [InlineData("020 7450 40005")]
-    [InlineData("+444 (0)20 7450 4000")]
-    [InlineData("123 123 123 123")]
-    [InlineData("+44 (0)20 7450 4002df")]
-    [InlineData("+44 (0)20 7450 abcd")]
-    [InlineData("+44")]
-    [InlineData("dd+44 (0)20 7450 400")]
-    public void ShouldReturnError_WhenValueIsInvalid(string? telephoneNumber)
+    [InlineData("+48 123 456 7890")]
+    [InlineData("+481234567890")]
+    [InlineData("+444 07771 900 900")]
+    [InlineData("+31 (0)20 7450 4000")]
+    [InlineData("+ (0)20 7450 4000")]
+    public void ShouldReturnUkTelephoneNumberValidationError_WhenValueIsInvalid(string? telephoneNumber)
     {
         // given
         var result = OperationResult.New();
@@ -51,5 +48,30 @@ public class IsValidTests
 
         // then
         result.HasValidationErrors.Should().BeTrue();
+        result.Errors.Should().ContainSingle(ValidationErrorMessage.EnterUkTelephoneNumber);
+    }
+
+    [Theory]
+    [InlineData("test")]
+    [InlineData(null)]
+    [InlineData("020 7450 40005")]
+    [InlineData("123 123 123 123")]
+    [InlineData("+44 (0)20 7450 4002df")]
+    [InlineData("+44 (0)20 7450 abcd")]
+    [InlineData("+44")]
+    [InlineData("dd+44 (0)20 7450 400")]
+    public void ShouldReturnTelephoneNumberFormatValidationError_WhenValueIsInvalid(string? telephoneNumber)
+    {
+        // given
+        var result = OperationResult.New();
+
+        // when
+        TelephoneNumberValidator
+            .For(telephoneNumber, "TelephoneNumber", "test", result)
+            .IsValid();
+
+        // then
+        result.HasValidationErrors.Should().BeTrue();
+        result.Errors.Should().ContainSingle(ValidationErrorMessage.EnterTelephoneNumberInValidFormat);
     }
 }
