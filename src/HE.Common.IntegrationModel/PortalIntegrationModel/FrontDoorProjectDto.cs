8ca459abf3452e93917c4a2eeff64bb043dfa7e9
Wojciech Suski <wojciech.suski@pwc.com>
2024-03-12 14:22:37 +0100

    Add IntentiontoMakeaProfit To Front Door Dto

--- src/HE.Common.IntegrationModel/PortalIntegrationModel/FrontDoorProjectDto.cs
+++ src/HE.Common.IntegrationModel/PortalIntegrationModel/FrontDoorProjectDto.cs
@@ -13,21 +13,22 @@ namespace HE.Common.IntegrationModel.PortalIntegrationModel
         public Guid OrganisationId { get; set; }
         public string ProjectName { get; set; }
         public bool? ProjectSupportsHousingDeliveryinEngland { get; set; }
-        public List<int> ActivitiesinThisProject { get; set; } 
+        public List<int> ActivitiesinThisProject { get; set; }
         public List<int> InfrastructureDelivered { get; set; }
-        public int? AmountofAffordableHomes { get; set; } 
+        public int? AmountofAffordableHomes { get; set; }
         public int? PreviousResidentialBuildingExperience { get; set; }
         public bool? IdentifiedSite { get; set; }
-        public int? GeographicFocus { get; set; } 
-        public List<int> Region { get; set; } 
+        public int? GeographicFocus { get; set; }
+        public List<int> Region { get; set; }
         public Guid LocalAuthority { get; set; }
         public int? NumberofHomesEnabledBuilt { get; set; }
         public bool? WouldyourprojectfailwithoutHEsupport { get; set; }
         public bool? FundingRequired { get; set; }
-        public int? AmountofFundingRequired { get; set; } 
+        public int? AmountofFundingRequired { get; set; }
         public int? StartofProjectMonth { get; set; }
         public int? StartofProjectYear { get; set; }
         public DateTime? CreatedOn { get; set; }
         public UserAccountDto FrontDoorProjectContact { get; set; }
-    }
+        public bool? IntentiontoMakeaProfit { get; set; }
+    } 
 }
