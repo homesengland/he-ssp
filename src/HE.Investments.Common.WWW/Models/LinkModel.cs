3f189047331694325f7f0dbd0630c9f50aacbbc7
Carq <lciura.pl@gmail.com>
2024-03-20 12:49:27 +0100

    AB#92119 Fix issues for local authority pages (#1231)

--- src/HE.Investments.Common.WWW/Models/LinkModel.cs
+++ src/HE.Investments.Common.WWW/Models/LinkModel.cs
@@ -1,3 +1,7 @@
 namespace HE.Investments.Common.WWW.Models;
 
-public record LinkModel(string Action, string Controller, object Values);
+public record LinkModel(ActionLinkModel ActionLink, DirectLinkModel DirectLink);
+
+public record ActionLinkModel(string Action, string Controller, object Values);
+
+public record DirectLinkModel(string DirectLink, bool OpenInNewTab = false);
