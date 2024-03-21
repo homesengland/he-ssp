afc1bfc295e49ba32cf38fb7ea54d1e76bc9bcf4
Łukasz Ciura <lukasz.ciura@pwc.com>
2024-03-21 09:01:32 +0100

    AB#91374 Resume Project and site workflow

--- src/HE.Investments.FrontDoor.WWW/Models/Factories/ProjectSummaryViewModelFactory.cs
+++ src/HE.Investments.FrontDoor.WWW/Models/Factories/ProjectSummaryViewModelFactory.cs
@@ -30,6 +30,11 @@ public class ProjectSummaryViewModelFactory : IProjectSummaryViewModelFactory
         {
             yield return new SectionSummaryViewModel(string.Empty, CreateProjectBasicDetailsSummary(projectDetails, CreateProjectAction, isEditable));
 
+            if (!projectSites.Sites.Any())
+            {
+                projectSites.Sites.Add(new SiteDetails());
+            }
+
             foreach (var siteSummary in CreateSitesSummary(urlHelper, projectDetails, projectSites, CreateProjectAction, isEditable, useWorkflowRedirection))
             {
                 yield return siteSummary;
@@ -61,16 +66,16 @@ public class ProjectSummaryViewModelFactory : IProjectSummaryViewModelFactory
 
         sectionSummary.AddWhen(
             new(
-            "Region",
-            projectDetails.Regions?.Select(x => x.GetDescription()).ToList(),
-            createAction(nameof(ProjectController.Region)),
-            IsEditable: isEditable),
+                "Region",
+                projectDetails.Regions?.Select(x => x.GetDescription()).ToList(),
+                createAction(nameof(ProjectController.Region)),
+                IsEditable: isEditable),
             projectDetails.GeographicFocus == ProjectGeographicFocus.Regional);
 
         sectionSummary.AddWhen(
             new(
                 "Local authority",
-                "Implement Local authority".ToOneElementList(),  // todo local authority
+                "Implement Local authority".ToOneElementList(), // todo local authority
                 createAction(nameof(ProjectController.LocalAuthoritySearch)),
                 IsEditable: isEditable),
             projectDetails.GeographicFocus == ProjectGeographicFocus.Regional);
@@ -147,7 +152,7 @@ public class ProjectSummaryViewModelFactory : IProjectSummaryViewModelFactory
                 sectionSummaryItems.AddRange(siteSummary.Items!);
             }
 
-            summaries.Add(new SectionSummaryViewModel(site.Name, sectionSummaryItems));
+            summaries.Add(new SectionSummaryViewModel(string.IsNullOrWhiteSpace(site.Name) ? "Site" : site.Name, sectionSummaryItems));
         }
 
         return summaries;
@@ -177,7 +182,7 @@ public class ProjectSummaryViewModelFactory : IProjectSummaryViewModelFactory
         CreateAction createAction,
         bool isEditable)
     {
-        return new List<SectionSummaryItemModel>
+        var summary = new List<SectionSummaryItemModel>
         {
             new(
                 "Project progress more slowly or stall",
@@ -189,22 +194,32 @@ public class ProjectSummaryViewModelFactory : IProjectSummaryViewModelFactory
                 SummaryAnswerHelper.ToYesNo(projectDetails.IsFundingRequired),
                 createAction(nameof(ProjectController.RequiresFunding)),
                 IsEditable: isEditable),
-            new(
-                "How much funding",
-                SummaryAnswerHelper.ToEnum(projectDetails.RequiredFunding),
-                createAction(nameof(ProjectController.FundingAmount)),
-                IsEditable: isEditable),
-            new(
-                "Intention to make a profit",
-                SummaryAnswerHelper.ToYesNo(projectDetails.IsProfit),
-                createAction(nameof(ProjectController.Profit)),
-                IsEditable: isEditable),
-            new(
-                "Expected project start date",
-                SummaryAnswerHelper.ToDate(projectDetails.ExpectedStartDate),
-                createAction(nameof(ProjectController.ExpectedStart)),
-                IsEditable: isEditable),
         };
+
+        if (projectDetails.IsFundingRequired == true)
+        {
+            summary.AddRange(new[]
+            {
+                new SectionSummaryItemModel(
+                    "How much funding",
+                    SummaryAnswerHelper.ToEnum(projectDetails.RequiredFunding),
+                    createAction(nameof(ProjectController.FundingAmount)),
+                    IsEditable: isEditable),
+                new SectionSummaryItemModel(
+                    "Intention to make a profit",
+                    SummaryAnswerHelper.ToYesNo(projectDetails.IsProfit),
+                    createAction(nameof(ProjectController.Profit)),
+                    IsEditable: isEditable),
+            });
+        }
+
+        summary.Add(new(
+            "Expected project start date",
+            SummaryAnswerHelper.ToDate(projectDetails.ExpectedStartDate),
+            createAction(nameof(ProjectController.ExpectedStart)),
+            IsEditable: isEditable));
+
+        return summary;
     }
 
     private static string CreateProjectActionUrl(IUrlHelper urlHelper, FrontDoorProjectId projectId, string actionName, bool useWorkflowRedirection)
@@ -217,13 +232,24 @@ public class ProjectSummaryViewModelFactory : IProjectSummaryViewModelFactory
         return action ?? string.Empty;
     }
 
-    private static string CreateSiteActionUrl(IUrlHelper urlHelper, FrontDoorProjectId projectId, FrontDoorSiteId siteId, string actionName, bool useWorkflowRedirection)
+    private static string CreateSiteActionUrl(
+        IUrlHelper urlHelper,
+        FrontDoorProjectId projectId,
+        FrontDoorSiteId? siteId,
+        string actionName,
+        bool useWorkflowRedirection)
     {
-        var action = urlHelper.Action(
+        if (siteId.IsNotProvided())
+        {
+            return urlHelper.Action(
+                nameof(SiteController.NewName),
+                new ControllerName(nameof(SiteController)).WithoutPrefix(),
+                new { projectId = projectId.Value, redirect = useWorkflowRedirection ? nameof(ProjectController.CheckAnswers) : null }) ?? string.Empty;
+        }
+
+        return urlHelper.Action(
             actionName,
             new ControllerName(nameof(SiteController)).WithoutPrefix(),
-            new { projectId = projectId.Value, siteId = siteId.Value, redirect = useWorkflowRedirection ? nameof(ProjectController.CheckAnswers) : null });
-
-        return action ?? string.Empty;
+            new { projectId = projectId.Value, siteId = siteId!.Value, redirect = useWorkflowRedirection ? nameof(ProjectController.CheckAnswers) : null }) ?? string.Empty;
     }
 }
