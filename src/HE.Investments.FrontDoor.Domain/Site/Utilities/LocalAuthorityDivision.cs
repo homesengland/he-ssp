fd7b45be897b75e7189930acfb2cc85b03e32aa9
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-03-22 12:58:49 +0100

    add warning disable S6605

--- src/HE.Investments.FrontDoor.Domain/Site/Utilities/LocalAuthorityDivision.cs
+++ src/HE.Investments.FrontDoor.Domain/Site/Utilities/LocalAuthorityDivision.cs
@@ -7,6 +7,8 @@ public static class LocalAuthorityDivision
 {
     public static bool IsLocalAuthorityNotAllowedForLoanApplication(string? localAuthorityCode)
     {
+#pragma warning disable S6605
         return Enum.GetValues<NotAllowedLocalAuthority>().Any(x => x.GetDescription() == localAuthorityCode);
+#pragma warning restore S6605
     }
 }
