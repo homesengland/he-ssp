b3716937ebd68be6418a03ba12d56fb008f09a5e
Jakub Jankowski <jakub.jankowski1987@gmail.com>
2024-03-22 12:27:50 +0100

    Increase accounts test coverage.

--- src/HE.Investments.Account.Domain.Tests/UserOrganisation/CommandHandlers/InviteUserToOrganisationCommandHandlerTests.cs
+++ src/HE.Investments.Account.Domain.Tests/UserOrganisation/CommandHandlers/InviteUserToOrganisationCommandHandlerTests.cs
@@ -1,8 +1,16 @@
 using FluentAssertions;
+using HE.Investments.Account.Api.Contract.User;
 using HE.Investments.Account.Contract.UserOrganisation.Commands;
+using HE.Investments.Account.Domain.Tests.UserOrganisation.TestDataBuilders;
 using HE.Investments.Account.Domain.UserOrganisation.CommandHandlers;
+using HE.Investments.Account.Domain.UserOrganisation.Repositories;
+using HE.Investments.Account.Shared;
+using HE.Investments.Account.Shared.User.ValueObjects;
+using HE.Investments.Common.Contract;
 using HE.Investments.Common.Contract.Validators;
+using HE.Investments.Common.Tests.TestData;
 using HE.Investments.TestsUtils.TestFramework;
+using Moq;
 using Xunit;
 
 namespace HE.Investments.Account.Domain.Tests.UserOrganisation.CommandHandlers;
@@ -10,7 +18,7 @@ namespace HE.Investments.Account.Domain.Tests.UserOrganisation.CommandHandlers;
 public class InviteUserToOrganisationCommandHandlerTests : TestBase<InviteUserToOrganisationCommandHandler>
 {
     [Fact]
-    public async Task ShouldReturnValidationErrors_WhenUserDataIsNotValid()
+    public async Task ShouldReturnValidationErrors_WhenUserDataIsNotProvided()
     {
         // given
         var command = new InviteUserToOrganisationCommand(
@@ -33,5 +41,25 @@ public class InviteUserToOrganisationCommandHandlerTests : TestBase<InviteUserTo
             .And.Contain(new ErrorItem("EmailAddress", "Enter the email address"))
             .And.Contain(new ErrorItem("JobTitle", "Enter the job title"))
             .And.Contain(new ErrorItem("Role", "Select the role"));
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessResult_WhenUserDataProvided()
+    {
+        // given
+        var command = new InviteUserToOrganisationCommand("John", "Paul", "john.paul@he.gov.uk", "Pope", UserRole.Enhanced);
+        var userContext = CreateAndRegisterDependencyMock<IAccountUserContext>();
+        var organisationUsersRepository = CreateAndRegisterDependencyMock<IOrganisationUsersRepository>();
+
+        userContext.Setup(x => x.GetSelectedAccount()).ReturnsAsync(UserAccountTestData.UserAccountOne);
+        organisationUsersRepository.Setup(x => x.GetOrganisationUsers(It.IsAny<OrganisationId>(), CancellationToken.None))
+            .ReturnsAsync(new OrganisationUsersEntityTestDataBuilder().Build());
+
+        // when
+        var result = await TestCandidate.Handle(command, CancellationToken.None);
+
+        // then
+        result.Should().NotBeNull();
+        result.HasValidationErrors.Should().BeFalse();
     }
 }
