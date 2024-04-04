aa9d6b8c37e885a70b505266e3dca36b0f73ba8d
Łukasz Ciura <lukasz.ciura@pwc.com>
2024-04-04 15:15:18 +0200

    AB#94094 Parrarel tests

--- src/HE.Investments.Loans.IntegrationTests/AssemblyInfo.cs
+++ src/HE.Investments.Loans.IntegrationTests/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 using Xunit;
 
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
 [assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]
 [assembly: TestCollectionOrderer("Xunit.Extensions.Ordering.CollectionOrderer", "Xunit.Extensions.Ordering")]
 [assembly: TestFramework("Xunit.Extensions.Ordering.TestFramework", "Xunit.Extensions.Ordering")]
