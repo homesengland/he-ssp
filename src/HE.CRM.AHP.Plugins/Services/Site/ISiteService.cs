dffbd0f792869313843a2f143dbf8559bc6e6c20
Paweł Bargielski <pawel.bargielski@pwc.com>
2024-04-08 01:26:29 +0200

    AB#94641 - Extension of endpoints invln_setsite and invln_getmultiplesites by fields externalContactId and accountId

--- src/HE.CRM.AHP.Plugins/Services/Site/ISiteService.cs
+++ src/HE.CRM.AHP.Plugins/Services/Site/ISiteService.cs
@@ -6,12 +6,12 @@ namespace HE.CRM.AHP.Plugins.Services.Site
 {
     public interface ISiteService : ICrmService
     {
-        PagedResponseDto<SiteDto> Get(PagingRequestDto paging, string fieldsToRetrieve);
+        PagedResponseDto<SiteDto> GetMultiple(PagingRequestDto paging, string fieldsToRetrieve, string externalContactId, string accountId);
 
         SiteDto GetById(string id, string fieldsToRetrieve);
 
         bool Exist(string name);
 
-        string Save(string siteId, SiteDto site, string fieldsToSet);
+        string Save(string siteId, SiteDto site, string fieldsToSet, string externalContactId, string accountId);
     }
 }
