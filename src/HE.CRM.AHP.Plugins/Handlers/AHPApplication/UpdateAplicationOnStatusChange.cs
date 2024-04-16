ece5568b1e7d40f9438e7db611266eb91a369c08
Wojciech Suski <wojciech.suski@pwc.com>
2024-04-16 15:20:50 +0200

    under develop

--- src/HE.CRM.AHP.Plugins/Handlers/AHPApplication/UpdateAplicationOnStatusChange.cs
+++ src/HE.CRM.AHP.Plugins/Handlers/AHPApplication/UpdateAplicationOnStatusChange.cs
@@ -1,11 +1,20 @@
+using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using DataverseModel;
 using HE.Base.Plugins.Handlers;
+using HE.CRM.Common.Repositories.Interfaces;
 
 namespace HE.CRM.AHP.Plugins.Handlers.AHPApplication
 {
     public class UpdateAplicationOnStatusChange : CrmEntityHandlerBase<invln_scheme, DataverseContext>
     {
+        private readonly IHomeTypeRepository _homeTypeRepository;
+
+        public UpdateAplicationOnStatusChange(IHomeTypeRepository _homeTypeRepository)
+        {
+            this._homeTypeRepository = _homeTypeRepository;
+        }
+
         public override bool CanWork()
         {
             return ValueChanged(invln_scheme.Fields.StatusCode);
@@ -21,7 +30,12 @@ namespace HE.CRM.AHP.Plugins.Handlers.AHPApplication
             }
             if (CurrentState.StatusCode.Value == (int)invln_scheme_StatusCode.ApplicationSubmitted)
             {
-                var hometype =
+                var hometype = _homeTypeRepository.GetByAttribute(invln_HomeType.Fields.invln_application,
+                    CurrentState.Id, new string[] { invln_HomeType.Fields.invln_PercentageValueofNDSSStandard });
+                if (hometype == null || hometype.Count == 0)
+                    return;
+                var percentageValueofNDSSStandardMax = hometype.MaxBy(x => x.)
+                //var percentageValueofNDSSStandardMin
             }
 
         }
