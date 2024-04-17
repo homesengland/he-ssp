f3a42e772e79c66f10a2babc9cd628a8db51c5fd
Wojciech Suski <wojciech.suski@pwc.com>
2024-04-17 11:22:41 +0200

    #AB90866 Rebuild model

--- src/HE.CRM.Model/Settings/EntityOptionSetEnum.cs
+++ src/HE.CRM.Model/Settings/EntityOptionSetEnum.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HE.Investments.Common.CRM.Model
+namespace DataverseModel
 {
 	
 	
