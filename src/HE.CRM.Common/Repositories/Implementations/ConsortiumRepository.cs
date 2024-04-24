2725ce5e7940365659494779ea8a4029314d7a47
Wojciech Suski <wojciech.suski@pwc.com>
2024-04-24 12:51:30 +0200

    Add check IsConsortiumExist

--- src/HE.CRM.Common/Repositories/Implementations/ConsortiumRepository.cs
+++ src/HE.CRM.Common/Repositories/Implementations/ConsortiumRepository.cs
@@ -15,5 +15,24 @@ namespace HE.CRM.Common.Repositories.Implementations
         public ConsortiumRepository(CrmRepositoryArgs args) : base(args)
         {
         }
+
+        public bool GetByProgrammeAndOrganization(Guid programmeId, Guid organizationId)
+        {
+            var query_1 = new QueryExpression(invln_Consortium.EntitySchemaName);
+            query_1.ColumnSet = new ColumnSet(invln_Consortium.Fields.Id);
+            query_1.Criteria.AddCondition(invln_Consortium.Fields.invln_Programme, ConditionOperator.Equal, programmeId);
+            query_1.Criteria.AddCondition(invln_Consortium.Fields.invln_LeadPartner, ConditionOperator.Equal, organizationId);
+            var response_1 = service.RetrieveMultiple(query_1);
+
+            var query_2 = new QueryExpression(invln_Consortium.EntitySchemaName)
+            {
+                ColumnSet = new ColumnSet(invln_Consortium.Fields.Id)
+            };
+            query_2.Criteria.AddCondition(invln_Consortium.Fields.invln_Programme, ConditionOperator.Equal, programmeId);
+            var query_invln_consortiummember = query_2.AddLink(invln_ConsortiumMember.EntityLogicalName, invln_Consortium.Fields.invln_ConsortiumId, invln_ConsortiumMember.Fields.invln_Consortium);
+            query_invln_consortiummember.LinkCriteria.AddCondition(invln_ConsortiumMember.Fields.invln_Partner, ConditionOperator.Equal, organizationId);
+            var response_2 = service.RetrieveMultiple(query_2);
+            return response_1.Entities.Count > 0 || response_2.Entities.Count > 0;
+        }
     }
 }
--- src/HE.CRM.Common/Repositories/Interfaces/IConsortiumrepository.cs
+++ src/HE.CRM.Common/Repositories/Interfaces/IConsortiumrepository.cs
@@ -1,14 +1,12 @@
 using System;
-using System.Collections.Generic;
-using Microsoft.Xrm.Sdk.Query;
-using HE.Base.Repositories;
 using DataverseModel;
-using Microsoft.Xrm.Sdk;
+using HE.Base.Repositories;
 
 namespace HE.CRM.Common.Repositories.Interfaces
 {
 
     public interface IConsortiumRepository : ICrmEntityRepository<invln_Consortium, DataverseContext>
     {
+        bool GetByProgrammeAndOrganization(Guid programmeId, Guid organizationId);
     }
 }
