25994fb9946e23b28f4dc9e900c25189f67fca1a
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-05-07 11:10:58 +0200

    change return to your account

--- src/HE.Investment.AHP.WWW/Controllers/AccountController.cs
+++ src/HE.Investment.AHP.WWW/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 using HE.Investments.Account.Shared.Routing;
+using HE.Investments.Common;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
 
 namespace HE.Investment.AHP.WWW.Controllers;
 
@@ -8,14 +10,22 @@ public class AccountController : Controller
 {
     private readonly AccountConfig _accountConfig;
 
-    public AccountController(AccountConfig accountConfig)
+    private readonly IFeatureManager _featureManager;
+
+    public AccountController(AccountConfig accountConfig, IFeatureManager featureManager)
     {
         _accountConfig = accountConfig;
+        _featureManager = featureManager;
     }
 
     [HttpGet]
-    public IActionResult Index()
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
+        if (await _featureManager.IsEnabledAsync(FeatureFlags.StayInCurrentApplication, cancellationToken))
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
         return new RedirectResult(_accountConfig.Url);
     }
 }
