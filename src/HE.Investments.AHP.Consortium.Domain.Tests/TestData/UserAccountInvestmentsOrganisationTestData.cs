31cb18046e6aac0e11f293fec032a7e8d53803da
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-05-08 09:48:36 +0200

    add unit tests for GetConsortiumsListQueryHandler

---
+++ src/HE.Investments.AHP.Consortium.Domain.Tests/TestData/UserAccountInvestmentsOrganisationTestData.cs
@@ -0,0 +1,15 @@
+using HE.Investments.Account.Api.Contract.User;
+using HE.Investments.Account.Shared;
+using HE.Investments.Account.Shared.User;
+using HE.Investments.Common.Contract;
+
+namespace HE.Investments.AHP.Consortium.Domain.Tests.TestData;
+
+public class UserAccountInvestmentsOrganisationTestData
+{
+    public static readonly UserAccount JjCompanyEmployee = new(
+        UserGlobalId.From("MasterJ"),
+        "User@company.com",
+        new OrganisationBasicInfo(InvestmentsOrganisationTestData.JjCompany.Id, "AccountOne", "1234", "London", false),
+        [UserRole.Admin, UserRole.Limited]);
+}
