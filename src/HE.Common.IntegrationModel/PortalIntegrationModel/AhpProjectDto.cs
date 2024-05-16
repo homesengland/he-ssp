cc0f5430c22a3d75cd48110e40025846acc0c50d
PawelBargielski <156068678+PawelBargielski@users.noreply.github.com>
2024-05-16 12:09:23 +0200

    AB#97246 - Endpoint CreateAhpProject (#1498)

---
+++ src/HE.Common.IntegrationModel/PortalIntegrationModel/AhpProjectDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace HE.Common.IntegrationModel.PortalIntegrationModel
+{
+    public class AhpProjectDto
+    {
+        public string AhpProjectId { get; set; }
+        public string AhpProjectName { get; set; }
+        public List<SiteDto> ListOfSites { get; set; }
+        public List<AhpApplicationDto> ListOfApplications { get; set; }
+    }
+}
