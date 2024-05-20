e5697d9077994c83d52d7c3c40042a3781755c6a
suskiwojciech <156068168+suskiwojciech@users.noreply.github.com>
2024-05-20 14:12:21 +0200

    #AB97141 revert back default values and audit trail approver (#1494)

---
+++ src/HE.CRM.Model/CrmSerialiedParameters/AHP_DELIVERY_PHASE_NOTIFICATION_OF_ADJUSTMENT_ACCEPTED.cs
@@ -0,0 +1,17 @@
+namespace HE.CRM.Model.CrmSerialiedParameters
+{
+    public class AHP_DELIVERY_PHASE_NOTIFICATION_OF_ADJUSTMENT_ACCEPTED
+    {
+        public string templateId { get; set; }
+        public parameters_AHP_DELIVERY_PHASE_NOTIFICATION_OF_ADJUSTMENT_ACCEPTED personalisation { get; set; }
+    }
+
+    public class parameters_AHP_DELIVERY_PHASE_NOTIFICATION_OF_ADJUSTMENT_ACCEPTED
+    {
+        // Required always
+        public string subject { get; set; }
+        public string recipientEmail { get; set; }
+        // Additional
+        public string name { get; set; }
+    }
+}
