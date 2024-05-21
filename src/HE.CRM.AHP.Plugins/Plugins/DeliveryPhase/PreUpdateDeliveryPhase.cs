4f9c4bc010f5ceac2ee9554de4ba7a1762b2a676
Wojciech Suski <wojciech.suski@pwc.com>
2024-05-21 14:32:37 +0200

    #AB98239 Fix

--- src/HE.CRM.AHP.Plugins/Plugins/DeliveryPhase/PreUpdateDeliveryPhase.cs
+++ src/HE.CRM.AHP.Plugins/Plugins/DeliveryPhase/PreUpdateDeliveryPhase.cs
@@ -13,20 +13,21 @@ namespace HE.CRM.AHP.Plugins.Plugins.DeliveryPhase
         invln_DeliveryPhase.EntityLogicalName,
         StageEnum.PreOperation,
         ExecutionModeEnum.Synchronous,
-        invln_DeliveryPhase.Fields.StatusCode,
+        invln_DeliveryPhase.Fields.StatusCode + "," + invln_DeliveryPhase.Fields.invln_NoofHomes,
         "HE.CRM.AHP.Plugins.Plugins.DeliveryPhase.PreUpdateDeliveryPhase : PreUpdate of Delivery Phase",
         1,
         IsolationModeEnum.Sandbox,
         Id = "acb6edf2-59bf-42fd-8416-74ba9b55e51a",
         Image1Name = "PreImage",
         Image1Attributes = invln_DeliveryPhase.Fields.StatusCode + "," + invln_DeliveryPhase.Fields.invln_Application + "," +
-        invln_DeliveryPhase.Fields.ModifiedBy,
+        invln_DeliveryPhase.Fields.ModifiedBy + "," + invln_DeliveryPhase.Fields.invln_NoofHomes,
         Image1Type = ImageTypeEnum.PreImage)]
     public class PreUpdateDeliveryPhase : PluginBase<DataverseContext>, IPlugin
     {
         public override void RegisterHandlers(CrmHandlerFactory<DataverseContext> handlerFactory, IList<ICrmHandler> registeredHandlers)
         {
             registeredHandlers.Add(handlerFactory.GetHandler<AcceptOrRejectPaymentProportion>());
+            registeredHandlers.Add(handlerFactory.GetHandler<ChangeNumberOfHomes>());
         }
     }
 }
