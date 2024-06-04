235c2ecdaaa661cd73f9ebbaba47d8bf974bdf96
Wojciech Suski <wojciech.suski@pwc.com>
2024-06-04 10:10:23 +0200

    Add triger to update nomber of house

---
+++ src/HE.CRM.AHP.Plugins/Plugins/HomesInDeliveryPhase/PostUpdate.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using DataverseModel;
+using HE.Base.Plugins;
+using HE.Base.Plugins.Handlers;
+using HE.CRM.AHP.Plugins.Handlers.HomesInDeliveryPhases;
+using Microsoft.Xrm.Sdk;
+
+namespace HE.CRM.AHP.Plugins.Plugins.HomesInDeliveryPhase
+{
+    [CrmPluginRegistration(
+        MessageNameEnum.Update,
+        invln_HomeType.EntityLogicalName,
+        StageEnum.PostOperation,
+        ExecutionModeEnum.Synchronous,
+        "",
+        "HE.CRM.Plugins.Plugins.HomeType.HomesInDeliveryPhase: Update of Homes In Delivery Phase",
+        1,
+        IsolationModeEnum.Sandbox,
+        Id = "c27cc842-497e-4ba7-bcd3-b853296573ba",
+        Image1Name = "PreImage",
+        Image1Attributes = "",
+        Image1Type = ImageTypeEnum.PreImage
+    )]
+    public class PostUpdate : PluginBase<DataverseContext>, IPlugin
+    {
+        public override void RegisterHandlers(CrmHandlerFactory<DataverseContext> handlerFactory, IList<ICrmHandler> registeredHandlers)
+        {
+            registeredHandlers.Add(handlerFactory.GetHandler<CalculateNumberOfHouseForDeliveryPhase>());
+        }
+    }
+}
