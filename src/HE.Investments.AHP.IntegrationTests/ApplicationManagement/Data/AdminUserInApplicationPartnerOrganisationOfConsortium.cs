c394f942f36417b01c354d48c5458107796e9374
Carq <lciura.pl@gmail.com>
2024-06-07 09:04:41 +0200

    AB#98137 Application Management Integration tests part 4 - fix (#1632)

--- src/HE.Investments.AHP.IntegrationTests/ApplicationManagement/Data/AdminUserInApplicationPartnerOrganisationOfConsortium.cs
+++ src/HE.Investments.AHP.IntegrationTests/ApplicationManagement/Data/AdminUserInApplicationPartnerOrganisationOfConsortium.cs
@@ -6,7 +6,7 @@ public static class AdminUserInApplicationPartnerOrganisationOfConsortium
 {
     public static UserData UserData { get; } = new(
         "admin.consortium.application.partner-app01-integration.tests@gmail.com",
-        "auth0|665c43a9e627bbe9ed3b2fcf",
+        "auth0|665c46c63f7b6f5a7d5d2ad1",
         "03da109a-c920-ef11-840a-000d3a0cff9a",
         "ee269470-c620-ef11-840a-002248c83519");
 
