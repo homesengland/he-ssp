7ebd50f0c05d5dd338f0cc1bf262d2f88e9da50d
JJ <jakub.jankowski1987@gmail.com>
2024-06-07 14:46:11 +0200

    AB#99314 Add Document service health check endpoints + fix Redis HC. (#1642)

--- src/HE.Investments.Common.WWW/Infrastructure/HealthChecks/Connectors/RedisHealthCheckConnector.cs
+++ src/HE.Investments.Common.WWW/Infrastructure/HealthChecks/Connectors/RedisHealthCheckConnector.cs
@@ -1,25 +1,34 @@
-using HE.Investments.Common.Infrastructure.Cache.Config;
-using HealthChecks.Redis;
+using System.Diagnostics.CodeAnalysis;
+using HE.Investments.Common.Infrastructure.Cache.Interfaces;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 
 namespace HE.Investments.Common.WWW.Infrastructure.HealthChecks.Connectors;
 
 public sealed class RedisHealthCheckConnector : IHealthCheck
 {
-    private readonly ICacheConfig _cacheConfig;
+    private readonly ICacheService _cacheService;
 
-    public RedisHealthCheckConnector(ICacheConfig cacheConfig)
+    private readonly ILogger<RedisHealthCheckConnector> _logger;
+
+    public RedisHealthCheckConnector(ICacheService cacheService, ILogger<RedisHealthCheckConnector> logger)
     {
-        _cacheConfig = cacheConfig;
+        _cacheService = cacheService;
+        _logger = logger;
     }
 
-    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    [SuppressMessage("Design", "CA1031: Do not catch general exception types", Justification = "We need to catch all exceptions from Cache service in Health Check")]
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        if (string.IsNullOrEmpty(_cacheConfig.RedisConnectionString) || _cacheConfig.RedisConnectionString == "off")
+        try
         {
-            return HealthCheckResult.Healthy("Redis is disabled.");
+            _cacheService.GetValue<string>("health-check-key");
+            return Task.FromResult(HealthCheckResult.Healthy());
         }
-
-        return await new RedisHealthCheck(_cacheConfig.RedisConnectionString).CheckHealthAsync(context, cancellationToken);
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"{nameof(RedisHealthCheckConnector)} failed");
+            return Task.FromResult(HealthCheckResult.Unhealthy("Cache service connection is not available"));
+        }
     }
 }
