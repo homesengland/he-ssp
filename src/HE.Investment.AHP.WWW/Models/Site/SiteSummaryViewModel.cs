af9ba35379afb9172aad33189ca81be30d7407f9
JJ <jakub.jankowski1987@gmail.com>
2024-06-07 12:58:56 +0200

    AB#99665 Add Site status to check answers page. (#1637)

--- src/HE.Investment.AHP.WWW/Models/Site/SiteSummaryViewModel.cs
+++ src/HE.Investment.AHP.WWW/Models/Site/SiteSummaryViewModel.cs
@@ -1,19 +1,23 @@
 using HE.Investment.AHP.Contract.Common.Enums;
+using HE.Investment.AHP.Contract.Site;
 using HE.Investments.Common.WWW.Models.Summary;
 
 namespace HE.Investment.AHP.WWW.Models.Site;
 
 public class SiteSummaryViewModel : ISummaryViewModel
 {
-    public SiteSummaryViewModel(string siteId, IsSectionCompleted? isSectionCompleted, IList<SectionSummaryViewModel> sections, bool isEditable)
+    public SiteSummaryViewModel(string siteId, SiteStatus siteStatus, IsSectionCompleted? isSectionCompleted, IList<SectionSummaryViewModel> sections, bool isEditable)
     {
         SiteId = siteId;
+        SiteStatus = siteStatus;
         IsSectionCompleted = isSectionCompleted;
         Sections = sections;
         IsEditable = isEditable;
     }
 
     public string SiteId { get; }
+
+    public SiteStatus SiteStatus { get; }
 
     public IsSectionCompleted? IsSectionCompleted { get; }
 
