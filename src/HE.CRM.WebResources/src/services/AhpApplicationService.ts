bc717a54d4610db9b9ffcc16beb214dde33eb2ea
Wojciech Suski <wojciech.suski@pwc.com>
2024-06-17 12:27:29 +0200

    under develop

--- src/HE.CRM.WebResources/src/services/AhpApplicationService.ts
+++ src/HE.CRM.WebResources/src/services/AhpApplicationService.ts
@@ -7,6 +7,23 @@ export class AhpApplicationService {
     this.common = new CommonLib(eCtx)
   }
 
+  public ndssCalculationError() {
+    let numberofbedrooms = this.common.getAttribute("invln_numberofbedrooms").getValue();
+    console.log(numberofbedrooms);
+    let maxoccupancy = this.common.getAttribute("invln_maxoccupancy").getValue();
+    console.log(maxoccupancy);
+    let numberofstoreys = this.common.getAttribute("invln_numberofstoreys").getValue();
+    console.log(numberofstoreys);
+    if (numberofbedrooms == null || maxoccupancy == null || numberofstoreys == null) {
+      console.log("return");
+      return;
+    }
+    let concatenatevalue = numberofbedrooms.toString() + maxoccupancy.toString() + numberofstoreys.toString();
+
+    https://investmentsdev.crm11.dynamics.com/api/data/v9.2/invln_ndsses?$select=invln_standardnumber&$filter=(invln_standardnumber eq 111)&$top=50
+
+  }
+
   public openCustomPage() {
     var recordId = this.common.trimBraces(this.common.getCurrentEntityId())
     var pageInput: any = {
