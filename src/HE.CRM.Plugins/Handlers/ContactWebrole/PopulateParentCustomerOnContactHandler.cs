c4c4e695bace57d69dcfb7b87ad24a069756eecf
Marcin Baczewski <marcin.baczewski@pwc.com>
2024-06-18 14:30:23 +0200

    Revert assembly name, use an existing IContactRepository

--- src/HE.CRM.Plugins/Handlers/ContactWebrole/PopulateParentCustomerOnContactHandler.cs
+++ src/HE.CRM.Plugins/Handlers/ContactWebrole/PopulateParentCustomerOnContactHandler.cs
@@ -2,6 +2,7 @@ using DataverseModel;
 using HE.Base.Plugins.Attributes;
 using HE.Base.Plugins.Common.Constants;
 using HE.Base.Plugins.Handlers;
+using HE.CRM.Common.Repositories.Interfaces;
 using Microsoft.Xrm.Sdk;
 
 namespace HE.CRM.Plugins.Handlers.ContactWebrole
@@ -10,6 +11,13 @@ namespace HE.CRM.Plugins.Handlers.ContactWebrole
     [CrmProcessingStage(CrmProcessingStepStages.Postoperation)]
     public class PopulateParentCustomerOnContactHandler : CrmEntityHandlerBase<invln_contactwebrole, DataverseContext>
     {
+        private readonly IContactRepository _contactRepository;
+
+        public PopulateParentCustomerOnContactHandler(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
+
         public override bool CanWork()
         {
             return ExecutionData.Target.invln_Accountid != null
@@ -18,17 +26,15 @@ namespace HE.CRM.Plugins.Handlers.ContactWebrole
 
         public override void DoWork()
         {
-            var contactRepository = CrmRepositoriesFactory.GetBase<Contact, DataverseContext>();
-
             var contactColumns = new string[] { Contact.Fields.ParentCustomerId };
-            var contact = contactRepository.GetById(ExecutionData.Target.invln_Contactid, contactColumns);
+            var contact = _contactRepository.GetById(ExecutionData.Target.invln_Contactid, contactColumns);
             if (contact.ParentCustomerId != null)
             {
                 Logger.Trace("Contact has ParentCustomerId, skip execution");
                 return;
             }
 
-            contactRepository.Update(new Contact()
+            _contactRepository.Update(new Contact()
             {
                 Id = contact.Id,
                 ParentCustomerId = new EntityReference(Account.EntityLogicalName, ExecutionData.Target.invln_Accountid.Id)
