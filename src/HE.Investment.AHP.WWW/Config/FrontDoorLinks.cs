2289a1124755a21eaa0c770b426d78d30d3885c0
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-06-24 16:18:52 +0200

    update urls

--- src/HE.Investment.AHP.WWW/Config/FrontDoorLinks.cs
+++ src/HE.Investment.AHP.WWW/Config/FrontDoorLinks.cs
@@ -1,13 +1,21 @@
+using HE.Investments.Account.Shared;
+using HE.Investments.Common.Contract;
+
 namespace HE.Investment.AHP.WWW.Config;
 
 public class FrontDoorLinks : IFrontDoorLinks
 {
     private readonly IConfiguration _configuration;
 
-    public FrontDoorLinks(IConfiguration configuration)
+    private readonly OrganisationId _selectedOrganisationId;
+
+    public FrontDoorLinks(IConfiguration configuration, IAccountUserContext accountUserContext)
     {
         _configuration = configuration;
+        _selectedOrganisationId = accountUserContext.GetSelectedAccount().Result.SelectedOrganisationId();
     }
 
-    public string StartNewProject => _configuration.GetValue<string>("AppConfiguration:ProgrammeUrl:StartFrontDoorProject") ?? string.Empty;
+    public string StartNewProject => _configuration
+        .GetValue<string>("AppConfiguration:ProgrammeUrl:StartFrontDoorProject") ?? string.Empty
+        .Replace("{organisationId}", _selectedOrganisationId.ToString());
 }
