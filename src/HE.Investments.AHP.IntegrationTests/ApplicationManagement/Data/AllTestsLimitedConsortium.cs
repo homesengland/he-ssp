cd8979a403c43e4f010e221f8a8372816316d79e
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-06-25 18:59:05 +0200

    fix integration tests

--- src/HE.Investments.AHP.IntegrationTests/ApplicationManagement/Data/AllTestsLimitedConsortium.cs
+++ src/HE.Investments.AHP.IntegrationTests/ApplicationManagement/Data/AllTestsLimitedConsortium.cs
@@ -10,7 +10,7 @@ public static class AllTestsLimitedConsortium
 
     public static AhpSite SiteLead => new("de3363fb-c620-ef11-840a-002248c5d15f", "AhpProjectSite-Admin-Lead", [ApplicationLead]);
 
-    public static AhpApplication ApplicationLead => new("c8cd8015-c720-ef11-840a-002248c5d15f", "AhpApplication-Admin-Lead-01");
+    public static AhpApplication ApplicationLead => new("1e03553c-1033-ef11-8e4e-6045bd0b0565", "AhpApplication-Admin-Lead-01");
 
     public static AhpProject ProjectSitePartner { get; } = new("9af93cb8-c820-ef11-840a-000d3a0cff9a", "AhpProject-Admin-SitePartner", [SitePartner]);
 
@@ -21,5 +21,5 @@ public static class AllTestsLimitedConsortium
 
     public static AhpApplication ApplicationSitePartnerForSitePartner => new("792badd6-c820-ef11-840a-000d3a0cff9a", "AhpApplication-Admin-SitePartner-01");
 
-    public static AhpApplication ApplicationPartnerForSitePartner => new("b1a0f0e8-c920-ef11-840a-000d3a0cff9a", "AhpApplication-Admin-ApplicationPartner-02");
+    public static AhpApplication ApplicationPartnerForSitePartner => new("e3b94518-1233-ef11-8e4e-6045bd0b0565", "AhpApplication-Admin-ApplicationPartner-02");
 }
