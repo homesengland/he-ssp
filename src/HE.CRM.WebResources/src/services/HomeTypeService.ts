64f8659a1e06ba2b1c003d0d5fea357fb99f57ca
PawelBargielski <156068678+PawelBargielski@users.noreply.github.com>
2024-06-27 16:31:06 +0200

    AB#100485 - hiding sections on HomeType form (#1751)

--- src/HE.CRM.WebResources/src/services/HomeTypeService.ts
+++ src/HE.CRM.WebResources/src/services/HomeTypeService.ts
@@ -1,4 +1,5 @@
 import { CommonLib } from '../Common'
+import { Tenure } from '../OptionSet';
 
 export class HomeTypeService {
   common: CommonLib
@@ -22,6 +23,30 @@ export class HomeTypeService {
     let ndss = await Xrm.WebApi.retrieveMultipleRecords("invln_ndss", "?$select=invln_standardnumber&$filter=(invln_standardnumber eq " + concatenatevalue + ")&$top=50");
     if (ndss.entities.length == 0) {
       this.common.openConfirmDialog("Home type not covered by NDSS.", " NDSS Alert.");
+    }
+  }
+
+  public async showHideSection() {
+    console.log("Execute showHideSection function");
+    let applicationId = this.common.getAttributeValue("invln_application");
+    let application = await Xrm.WebApi.retrieveRecord('invln_scheme', applicationId![0]['id']);
+    let tenur = application.invln_tenure;
+
+    if(tenur == Tenure.SharedOwnership || tenur == Tenure.OPSO || tenur == Tenure.HOLD) {
+      console.log("show : Overview", "Shared Ownership, OPSO and HOLD");
+      this.common.showSection("Overview", "Shared Ownership, OPSO and HOLD");
+    }
+    if(tenur == Tenure.RentRoBuy) {
+      console.log("show : Overview", "Rent to Buy Details");
+      this.common.showSection("Overview", "Rent to Buy Details");
+    }
+    if(tenur == Tenure.SocialRent) {
+      console.log("show : Overview", "Social Rent Details");
+      this.common.showSection("Overview", "Social Rent Details");
+    }
+    if(tenur == Tenure.AffordableRent) {
+      console.log("show : Overview", "Affordable Rent Details");
+      this.common.showSection("Overview", "Affordable Rent Details");
     }
   }
 }
