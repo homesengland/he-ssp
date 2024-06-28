2ca2a85b00beb0cd64eec3f91bbff72c780b7974
dawid-pawulski <130044868+dawid-pawulski@users.noreply.github.com>
2024-06-28 09:19:05 +0200

    AB#99754 Change Project name in view applications (#1760)

--- src/HE.Investment.AHP.WWW/Views/Project/Const/ProjectPageTitles.cs
+++ src/HE.Investment.AHP.WWW/Views/Project/Const/ProjectPageTitles.cs
@@ -2,7 +2,7 @@ namespace HE.Investment.AHP.WWW.Views.Project.Const;
 
 public static class ProjectPageTitles
 {
-    public static string ApplicationList(string projectName) => $"{projectName}'s project";
+    public static string ApplicationList(string projectName) => $"{projectName}";
 
     public static string Start(string programmeFullName, string programmeShortName) => $"Apply for {programmeFullName} ({programmeShortName})";
 }
