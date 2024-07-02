dfa996302c02f39a638fef26d3e26c31bd1c8692
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-07-02 16:38:34 +0200

    update expected contribution error messages

--- src/HE.Investment.AHP.Domain/FinancialDetails/ValueObjects/ExpectedContributionFields.cs
+++ src/HE.Investment.AHP.Domain/FinancialDetails/ValueObjects/ExpectedContributionFields.cs
@@ -6,27 +6,27 @@ public enum ExpectedContributionFields
 {
     Undefined,
 
-    [Description("borrowing against the rental income for this scheme contribution value")]
+    [Description("amount you will contribute from borrowing against the rental income for this scheme")]
     RentalIncomeBorrowing,
 
-    [Description("cross subsidy from the sale of open market homes on this scheme contribution value")]
+    [Description("amount you will contribute from the cross subsidy from the sale of open market homes on this scheme")]
     SaleOfHomesOnThisScheme,
 
-    [Description("cross subsidy from the sale of open market homes on other schemes contribution value")]
+    [Description("amount you will contribute from cross subsidy from the sale of open market homes on other schemes")]
     SaleOfHomesOnOtherSchemes,
 
-    [Description("own resources contribution value")]
+    [Description("amount you intend to contribute from your own resources")]
     OwnResources,
 
-    [Description("Recycled Capital Grant Fund contribution value")]
+    [Description("amount you intend to contribute from your Recycled Capital Grant Fund")]
     RcgfContribution,
 
-    [Description("other capital sources contribution value")]
+    [Description("amount you will contribute from other capital sources")]
     OtherCapitalSources,
 
-    [Description("initial sales receipts from Shared Ownership homes contribution value")]
+    [Description("amount you will contribute from initial sales receipts from Shared Ownership homes")]
     SharedOwnershipSales,
 
-    [Description("transfer value of the homes on this scheme contribution value")]
+    [Description("transfer value of the homes on this scheme")]
     HomesTransferValue,
 }
