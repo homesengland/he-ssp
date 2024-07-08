6377979c1f70b97ec4691587826508632e0f65fd
Dawid Pawulski <dawid.pawulski@pwc.com>
2024-07-08 08:30:08 +0200

    Create project setup for Claims

---
+++ src/HE.Investment.AHP.Allocation.Domain/Claim/Entities/ClaimEntity.cs
@@ -0,0 +1,5 @@
+using HE.Investment.AHP.Allocation.Contract.Allocation;
+
+namespace HE.Investment.AHP.Allocation.Domain.Claim.Entities;
+
+public record ClaimEntity(AhpAllocationId AhpAllocationId);
---
+++ src/HE.Investment.AHP.WWW/Views/Claim/Const/ClaimPageTitles.cs
@@ -0,0 +1,6 @@
+namespace HE.Investment.AHP.WWW.Views.Claim.Const;
+
+public static class ClaimPageTitles
+{
+    public const string Summary = "Manage claims";
+}
