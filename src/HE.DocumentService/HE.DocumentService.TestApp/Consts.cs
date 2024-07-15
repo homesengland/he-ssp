6597756b73b8114dd929d3846af731bc8425c37d
Marcin Baczewski <marcin.baczewski@pwc.com>
2024-07-15 09:10:32 +0200

    Add PartitionId parameter to upload and get file methods to handle more then 5000 items on the SharePoint library.

---
+++ src/HE.DocumentService/HE.DocumentService.TestApp/Consts.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HE.DocumentService.TestApp;
+
+internal static class Consts
+{
+    public static string SPListTitle => "Some Test SharePoint Library";
+
+    public static string SPListAlias => "Some%20Test%20SharePoint%20Library";
+}
