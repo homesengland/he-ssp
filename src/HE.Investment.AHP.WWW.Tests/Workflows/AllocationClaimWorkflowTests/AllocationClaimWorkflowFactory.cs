4a2718deb1ee9c87ddc8a6c7be9a8e4498eef59c
Jakub Jankowski <jakub.jankowski1987@gmail.com>
2024-07-15 15:00:45 +0200

    AB#85084 Claim worklfow + controller.

---
+++ src/HE.Investment.AHP.WWW.Tests/Workflows/AllocationClaimWorkflowTests/AllocationClaimWorkflowFactory.cs
@@ -0,0 +1,27 @@
+using HE.Investment.AHP.WWW.Workflows;
+using HE.Investments.AHP.Allocation.Contract.Claims;
+using HE.Investments.AHP.Allocation.Contract.Claims.Enum;
+using HE.Investments.Common.Contract;
+
+namespace HE.Investment.AHP.WWW.Tests.Workflows.AllocationClaimWorkflowTests;
+
+internal static class AllocationClaimWorkflowFactory
+{
+    public static AllocationClaimWorkflow Create(
+        MilestoneType milestoneType,
+        bool canBeClaimed,
+        AllocationClaimWorkflowState state = AllocationClaimWorkflowState.CheckAnswers)
+    {
+        var model = new MilestoneClaim(
+            milestoneType,
+            MilestoneStatus.Due,
+            1000000m,
+            0.3m,
+            new DateDetails("01", "01", "2025"),
+            null,
+            null,
+            canBeClaimed);
+
+        return new AllocationClaimWorkflow(state, model);
+    }
+}
