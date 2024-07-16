77efc5d3591210bc4890a0db144e3a2a65da2f60
Jakub Jankowski <jakub.jankowski1987@gmail.com>
2024-07-16 10:06:51 +0200

    AB#85084 Build fix.

--- src/HE.Investments.AHP.Allocation.Contract/Claims/Commands/ProvideCostsIncurredCommand.cs
+++ src/HE.Investments.AHP.Allocation.Contract/Claims/Commands/ProvideCostsIncurredCommand.cs
@@ -1,7 +1,6 @@
 using HE.Investments.AHP.Allocation.Contract.Claims.Enum;
-using HE.Investments.Common.Contract.Validators;
-using MediatR;
 
 namespace HE.Investments.AHP.Allocation.Contract.Claims.Commands;
 
-public sealed record ProvideCostsIncurredCommand(AllocationId AllocationId, PhaseId PhaseId, MilestoneType MilestoneType, bool? CostsIncurred) : IRequest<OperationResult>, IProvideClaimDetailsCommand;
+public sealed record ProvideCostsIncurredCommand(AllocationId AllocationId, PhaseId PhaseId, MilestoneType MilestoneType, bool? CostsIncurred)
+    : IProvideClaimDetailsCommand;
