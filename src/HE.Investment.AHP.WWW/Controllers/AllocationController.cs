072ec0030cadf709d9f4706e85f1a2cdd9f97e21
Łukasz Ciura <lukasz.ciura@pwc.com>
2024-07-25 12:54:07 +0200

    AB#103595 Allocation Overview - draft

--- src/HE.Investment.AHP.WWW/Controllers/AllocationController.cs
+++ src/HE.Investment.AHP.WWW/Controllers/AllocationController.cs
@@ -1,4 +1,7 @@
 using HE.Investments.Account.Shared.Authorization.Attributes;
+using HE.Investments.AHP.Allocation.Contract;
+using HE.Investments.AHP.Allocation.Contract.Overview;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HE.Investment.AHP.WWW.Controllers;
@@ -7,9 +10,17 @@ namespace HE.Investment.AHP.WWW.Controllers;
 [Route("{organisationId}/allocation/{allocationId}")]
 public class AllocationController : Controller
 {
-    [HttpGet("overview")]
-    public IActionResult Overview(string organisationId, string allocationId)
+    private readonly IMediator _mediator;
+
+    public AllocationController(IMediator mediator)
     {
-        return RedirectToAction("Summary", "AllocationClaims", new { organisationId, allocationId });
+        _mediator = mediator;
+    }
+
+    [HttpGet("overview")]
+    [HttpGet("")]
+    public async Task<IActionResult> Overview(string allocationId)
+    {
+        return View("Overview", await _mediator.Send(new GetAllocationOverviewQuery(AllocationId.From(allocationId))));
     }
 }
