d2f497de48bfaeeb2e5e7590e1876627a9bb5c61
Carq <lciura.pl@gmail.com>
2024-07-25 15:11:13 +0200

    AB#103595 Allocation Overview - draft (#1883)

---
+++ src/HE.Investments.AHP.Allocation.Contract/Overview/AllocationOverview.cs
@@ -0,0 +1,10 @@
+using HE.Investments.Common.Contract;
+
+namespace HE.Investments.AHP.Allocation.Contract.Overview;
+
+public record AllocationOverview(
+    AllocationBasicInfo BasicInfo,
+    ModificationDetails ModificationDetails,
+    bool IsInContract,
+    string OrganisationName,
+    string FdProjectId);
