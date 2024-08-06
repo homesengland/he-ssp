56d593ec179034a08bff9013879d389235fe71ec
Carq <lciura.pl@gmail.com>
2024-08-06 11:47:47 +0200

    AB#104410 Banner Notification contract (#1932)
    Co-authored-by: Leszek Szejka <leszek.szejka@homesengland.gov.uk>

---
+++ src/HE.UtilsService/HE.UtilsService.BannerNotification/Contract/BannerNotificationParameter.cs
@@ -0,0 +1,8 @@
+namespace HE.UtilsService.BannerNotification.Contract;
+
+public class BannerNotificationParameter
+{
+    public string Name { get; set; }
+
+    public string Value { get; set; }
+}
