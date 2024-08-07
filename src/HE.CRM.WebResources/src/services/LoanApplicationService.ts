3f8b31ba74b6530584a91eecd6ebe2f42252761c
Marcin Baczewski <marcin.baczewski@pwc.com>
2024-08-07 12:23:50 +0200

    .

--- src/HE.CRM.WebResources/src/services/LoanApplicationService.ts
+++ src/HE.CRM.WebResources/src/services/LoanApplicationService.ts
@@ -19,6 +19,7 @@ export class LoanApplicationService {
     let requireSectionForStatuses = [
       LoanAppInternalStatus.ReferredBacktoApplicant,
       LoanAppInternalStatus.SentforApproval,
+      LoanAppInternalStatus.UnderReview,
       LoanAppInternalStatus.NotApproved,
       LoanAppInternalStatus.ApprovedSubjecttoDueDiligence,
       LoanAppInternalStatus.ApplicationDeclined,
